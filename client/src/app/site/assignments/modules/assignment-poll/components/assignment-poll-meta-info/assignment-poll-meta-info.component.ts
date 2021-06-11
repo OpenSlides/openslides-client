f1320c495358ce9074f1dd15aa8b7d6d7c38fa51
Sean <sean.f.t.engelhardt@gmail.com>
2021-06-11 17:00:16 +0200

    Consequent enum of assignment detail
    Also fix all sorting lists
    Adjust naming of pro contra labels to be aligned to os3

--- client/src/app/site/assignments/modules/assignment-poll/components/assignment-poll-meta-info/assignment-poll-meta-info.component.ts
+++ client/src/app/site/assignments/modules/assignment-poll/components/assignment-poll-meta-info/assignment-poll-meta-info.component.ts
@@ -3,6 +3,7 @@ import { Component, Input } from '@angular/core';
 import { PollPropertyVerbose } from 'app/shared/models/poll/poll-constants';
 import { ViewOption } from 'app/shared/models/poll/view-option';
 import { ViewPoll } from 'app/shared/models/poll/view-poll';
+import { ViewAssignment } from 'app/site/assignments/models/view-assignment';
 import { UnknownUserLabel } from 'app/site/assignments/modules/assignment-poll/services/assignment-poll.service';
 
 @Component({
@@ -19,6 +20,14 @@ export class AssignmentPollMetaInfoComponent {
 
     @Input()
     public showCandidates = true;
+
+    private get assignment(): ViewAssignment {
+        return this.poll.content_object;
+    }
+
+    public get enumerateCandidates(): boolean {
+        return this.assignment?.number_poll_candidates || false;
+    }
 
     public get hasGlobalOptionEnabled(): boolean {
         return this.poll.hasGlobalOptionEnabled;
