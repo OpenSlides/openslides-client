e40e3681623da219e3de9c2a51ffaf6a2ecc5f7d
bug0002 <100357181+bug0002@users.noreply.github.com>
2022-02-25 13:20:22 +0100

    lint 808

--- client/src/app/shared/pipes/hfilesize.pipe.ts
+++ client/src/app/shared/pipes/hfilesize.pipe.ts
@@ -1,21 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-/**
- * Converts a file size in byte into human readable format
- *
- * @param bytes file size in bytes
- * @returns a readable file size representation
- */
 @Pipe({
     name: `readableBytes`
 })
 export class ReadableBytesPipe implements PipeTransform {
-  public transform(bytes: number): string {
-      if (bytes === 0) {
-          return `0 B`;
-      }
-      const unitLevel = Math.floor(Math.log(bytes) / Math.log(1024));
-      bytes = +(bytes / Math.pow(1024, unitLevel)).toFixed(2);
-      return `${bytes} ${[`B`, `kB`, `MB`, `GB`, `TB`][unitLevel]}`;
-  }
+    public transform(bytes: number): string {
+        if (bytes === 0) {
+            return `0 B`;
+        }
+        const unitLevel = Math.floor(Math.log(bytes) / Math.log(1024));
+        bytes = +(bytes / Math.pow(1024, unitLevel)).toFixed(2);
+        return `${bytes} ${[`B`, `kB`, `MB`, `GB`, `TB`][unitLevel]}`;
+    }
 }
