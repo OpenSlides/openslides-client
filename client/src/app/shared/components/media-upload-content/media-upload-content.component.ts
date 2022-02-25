d0d2b2fb08f951560ca5e3b02f2b3be03e413ba7
bug0002 <100357181+bug0002@users.noreply.github.com>
2022-02-25 08:37:15 +0100

    human readable fs 808

--- client/src/app/shared/components/media-upload-content/media-upload-content.component.ts
+++ client/src/app/shared/components/media-upload-content/media-upload-content.component.ts
@@ -160,20 +160,7 @@ export class MediaUploadContentComponent extends BaseModelContextComponent imple
         return file.type || `File`;
     }
 
-    /**
-     * Converts a file size in byte into human readable format
-     *
-     * @param bytes file size in bytes
-     * @returns a readable file size representation
-     */
-    public getReadableSize(bytes: number): string {
-        if (bytes === 0) {
-            return `0 B`;
-        }
-        const unitLevel = Math.floor(Math.log(bytes) / Math.log(1024));
-        bytes = +(bytes / Math.pow(1024, unitLevel)).toFixed(2);
-        return `${bytes} ${[`B`, `kB`, `MB`, `GB`, `TB`][unitLevel]}`;
-    }
+    
 
     /**
      * Change event to adjust the title
