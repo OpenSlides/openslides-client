292af21ac5225b888a23539b2f3996f5f5499092
GabrielInTheWorld <meyergabriel@live.de>
2022-03-30 12:57:15 +0200

    Fixes counting of active users (#913) (#976)

--- client/src/app/site/common/components/count-users/count-users.component.ts
+++ client/src/app/site/common/components/count-users/count-users.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnDestroy } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { SimplifiedModelRequest } from 'app/core/core-services/model-request-builder.service';
+import { ComponentServiceCollector } from 'app/core/ui-services/component-service-collector';
+import { ViewMeeting } from 'app/management/models/view-meeting';
+import { BaseModelContextComponent } from 'app/site/base/components/base-model-context.component';
 import { Observable } from 'rxjs';
 import { auditTime } from 'rxjs/operators';
 
@@ -11,11 +16,17 @@ import { CountUsersStatisticsService, CountUserStatistics } from '../../services
     selector: `os-count-users`,
     templateUrl: `./count-users.component.html`
 })
-export class CountUsersComponent implements OnDestroy {
+export class CountUsersComponent extends BaseModelContextComponent implements OnDestroy {
     public token: string = null;
     public stats: CountUserStatistics = null;
 
-    public constructor(private countUsersStatisticService: CountUsersStatisticsService) {}
+    public constructor(
+        componentServiceCollector: ComponentServiceCollector,
+        translate: TranslateService,
+        private countUsersStatisticService: CountUsersStatisticsService
+    ) {
+        super(componentServiceCollector, translate);
+    }
 
     public countUsers(): void {
         if (this.token) {
@@ -49,6 +60,16 @@ export class CountUsersComponent implements OnDestroy {
     }
 
     public ngOnDestroy(): void {
+        super.ngOnDestroy();
         this.stopCounting();
+    }
+
+    protected getModelRequest(): SimplifiedModelRequest {
+        return {
+            viewModelCtor: ViewMeeting,
+            ids: [this.activeMeetingId],
+            follow: [`group_ids`, { idField: `user_ids`, fieldset: `shortGroup` }],
+            fieldset: []
+        };
     }
 }
