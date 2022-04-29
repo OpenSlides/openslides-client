89c510642deb31965785cb4d6c67b37ee0870efe
Emanuel Schütze <emanuel@intevation.de>
2022-04-29 09:18:16 +0200

    Fixed linter

--- client/src/app/shared/components/editor/editor.component.ts
+++ client/src/app/shared/components/editor/editor.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewEncapsulation, AfterViewInit } from '@angular/core';
+import { AfterViewInit,Component, Input, ViewEncapsulation } from '@angular/core';
 import { EditorComponent } from '@tinymce/tinymce-angular';
 
 @Component({
