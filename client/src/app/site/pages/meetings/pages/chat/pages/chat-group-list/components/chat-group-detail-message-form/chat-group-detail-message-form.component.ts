061325edd8baaed8a8f59bc5060ebbb4dd0ebdd1
GabrielInTheWorld <meyergabriel@live.de>
2022-05-10 10:45:53 +0200

    Fixes the chat form (#1070)

---
+++ client/src/app/site/pages/meetings/pages/chat/pages/chat-group-list/components/chat-group-detail-message-form/chat-group-detail-message-form.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { CHAT_MESSAGE_MAX_LENGTH } from 'src/app/gateways/repositories/chat/chat-message-repository.service';
+import { FormControl, FormBuilder, Validators } from '@angular/forms';
+import { KeyCode } from 'src/app/infrastructure/utils/key-code';
+import { ViewChatMessage } from 'src/app/site/pages/meetings/pages/chat';
+
+@Component({
+    selector: 'os-chat-group-detail-message-form',
+    templateUrl: './chat-group-detail-message-form.component.html',
+    styleUrls: ['./chat-group-detail-message-form.component.scss']
+})
+export class ChatGroupDetailMessageFormComponent {
+    public readonly CHAT_MESSAGE_MAX_LENGTH = CHAT_MESSAGE_MAX_LENGTH;
+
+    @Input()
+    public set currentMessage(message: ViewChatMessage | null) {
+        this._currentMessage = message;
+        this.messageForm.setValue(message?.content || ``);
+    }
+
+    public get currentMessage(): ViewChatMessage | null {
+        return this._currentMessage;
+    }
+
+    @Output()
+    public messageSent = new EventEmitter<string>();
+
+    @Output()
+    public editingCanceled = new EventEmitter<void>();
+
+    public get isMessageFormValid(): boolean {
+        return !!this.messageForm.value?.trim()?.length && this.messageForm.valid;
+    }
+
+    public readonly messageForm: FormControl;
+
+    private _currentMessage: ViewChatMessage | null = null;
+
+    constructor(fb: FormBuilder) {
+        this.messageForm = fb.control(``, [Validators.maxLength(CHAT_MESSAGE_MAX_LENGTH)]);
+    }
+
+    public onKeyDown(event: KeyboardEvent): void {
+        if (event.key === KeyCode.ENTER) {
+            this.sendChatMessage();
+        }
+    }
+
+    public sendChatMessage(): void {
+        const content = this.messageForm.value?.trim() as string;
+        this.messageSent.emit(content);
+        this.resetMessageForm();
+    }
+
+    public cancelEditingChatMessage(): void {
+        this.editingCanceled.emit();
+        this.resetMessageForm();
+    }
+
+    private resetMessageForm(): void {
+        this.currentMessage = null;
+        this.messageForm.markAsPristine();
+    }
+}
