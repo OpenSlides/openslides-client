0feaf2710e90a3cb8fd6802bee37ce4b2dcddc30
Luisa <luisa.beerboom@intevation.de>
2022-06-01 16:10:47 +0200

    cleanup

--- client/src/app/domain/models/users/user.constants.ts
+++ client/src/app/domain/models/users/user.constants.ts
@@ -17,7 +17,7 @@ export const accountHeadersAndVerboseNames: { [key in keyof User]?: string } = {
     default_number: `Participant number`,
     default_structure_level: `Structure level`,
     default_vote_weight: `Vote weight`
-}
+};
 
 export const participantHeadersAndVerboseNames: { [key in keyof User]?: string } = {
     ...userHeadersAndVerboseNames,
