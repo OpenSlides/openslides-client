930a9e0fff426abca5b4e0e3e16e65969771598c
luisa-beerboom <101706784+luisa-beerboom@users.noreply.github.com>
2022-06-08 08:32:49 +0200

    poll-list now accessible (#1215)

--- client/src/app/site/pages/meetings/pages/polls/modules/poll-list/poll-list-routing.module.ts
+++ client/src/app/site/pages/meetings/pages/polls/modules/poll-list/poll-list-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PollListComponent } from './components/poll-list/poll-list.component';
+
+const routes: Routes = [
+    {
+        path: ``,
+        component: PollListComponent
+    }
+];
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
