2070ac54302568f28e4b282aecc780b802d90982
Magnus Schieder <magnus.schieder@intevation.de>
2022-07-13 17:20:22 +0200

    Prohibit incorrect jitsi url, Fix jitsi Buttons

--- client/src/app/site/pages/meetings/pages/interaction/modules/interaction-container/interaction-container.module.ts
+++ client/src/app/site/pages/meetings/pages/interaction/modules/interaction-container/interaction-container.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
@@ -39,6 +40,7 @@ const EXPORTS = [InteractionContainerComponent];
         MatDividerModule,
         MatTooltipModule,
         MatIconModule,
+        MatButtonModule,
         MatCardModule,
         NgParticlesModule,
         DirectivesModule,
