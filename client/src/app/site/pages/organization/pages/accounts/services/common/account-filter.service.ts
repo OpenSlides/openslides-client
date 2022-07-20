13042aae5a14acebbfc73304e302e5c6a9314a11
Emanuel Schütze <emanuel@intevation.de>
2022-07-20 19:14:08 +0200

    Hotfix: Remove committee and meeting filter from account filter.

--- client/src/app/site/pages/organization/pages/accounts/services/common/account-filter.service.ts
+++ client/src/app/site/pages/organization/pages/accounts/services/common/account-filter.service.ts
@@ -12,39 +12,14 @@ import { ActiveFiltersService } from 'src/app/site/services/active-filters.servi
 export class AccountFilterService extends BaseFilterListService<ViewUser> {
     protected storageKey = `MemberList`;
 
-    private committeeFilterOptions: OsFilter<ViewUser> = {
-        property: `committee_ids`,
-        label: this.translate.instant(`Committees`),
-        options: []
-    };
-
     public constructor(
-        committeeRepo: CommitteeRepositoryService,
         store: ActiveFiltersService,
-        private meetingRepo: MeetingControllerService,
         private translate: TranslateService
     ) {
         super(store);
-
-        this.updateFilterForRepo({
-            repo: committeeRepo,
-            filter: this.committeeFilterOptions,
-            noneOptionLabel: this.translate.instant(`No committee`)
-        });
     }
 
     protected getFilterDefinitions(): OsFilter<ViewUser>[] {
-        const meetingFilterOptions: OsFilterOptions = [
-            ...this.meetingRepo.getViewModelList().map(meeting => ({
-                condition: meeting.user_ids,
-                label: meeting.name
-            })),
-            `-`,
-            {
-                condition: null,
-                label: this.translate.instant(`No meeting`)
-            }
-        ];
         const staticFilterDefinitions: OsFilter<ViewUser>[] = [
             {
                 property: `is_active`,
@@ -70,12 +45,6 @@ export class AccountFilterService extends BaseFilterListService<ViewUser> {
                     { condition: true, label: this.translate.instant(`Has unchanged vote weight`) }
                 ]
             },
-            {
-                property: `id`,
-                label: this.translate.instant(`Meetings`),
-                options: meetingFilterOptions
-            },
-            this.committeeFilterOptions
         ];
         return staticFilterDefinitions;
     }
