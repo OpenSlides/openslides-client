12aca520a89b7938d98faaeec935480be1d85c28
Emanuel Schütze <emanuel@intevation.de>
2022-07-20 20:26:41 +0200

    Hotfix: Remove committee and meeting filter from account filter. (#1424)

--- client/src/app/site/pages/organization/pages/accounts/services/common/account-filter.service.ts
+++ client/src/app/site/pages/organization/pages/accounts/services/common/account-filter.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { CommitteeRepositoryService } from 'src/app/gateways/repositories/committee-repository.service';
-import { BaseFilterListService, OsFilter, OsFilterOptions } from 'src/app/site/base/base-filter.service';
-import { MeetingControllerService } from 'src/app/site/pages/meetings/services/meeting-controller.service';
+import { BaseFilterListService, OsFilter } from 'src/app/site/base/base-filter.service';
 import { ViewUser } from 'src/app/site/pages/meetings/view-models/view-user';
 import { ActiveFiltersService } from 'src/app/site/services/active-filters.service';
 
@@ -12,39 +10,11 @@ import { ActiveFiltersService } from 'src/app/site/services/active-filters.servi
 export class AccountFilterService extends BaseFilterListService<ViewUser> {
     protected storageKey = `MemberList`;
 
-    private committeeFilterOptions: OsFilter<ViewUser> = {
-        property: `committee_ids`,
-        label: this.translate.instant(`Committees`),
-        options: []
-    };
-
-    public constructor(
-        committeeRepo: CommitteeRepositoryService,
-        store: ActiveFiltersService,
-        private meetingRepo: MeetingControllerService,
-        private translate: TranslateService
-    ) {
+    public constructor(store: ActiveFiltersService, private translate: TranslateService) {
         super(store);
-
-        this.updateFilterForRepo({
-            repo: committeeRepo,
-            filter: this.committeeFilterOptions,
-            noneOptionLabel: this.translate.instant(`No committee`)
-        });
     }
 
     protected getFilterDefinitions(): OsFilter<ViewUser>[] {
-        const meetingFilterOptions: OsFilterOptions = [
-            ...this.meetingRepo.getViewModelList().map(meeting => ({
-                condition: meeting.user_ids,
-                label: meeting.name
-            })),
-            `-`,
-            {
-                condition: null,
-                label: this.translate.instant(`No meeting`)
-            }
-        ];
         const staticFilterDefinitions: OsFilter<ViewUser>[] = [
             {
                 property: `is_active`,
@@ -69,13 +39,7 @@ export class AccountFilterService extends BaseFilterListService<ViewUser> {
                     { condition: [false, null], label: this.translate.instant(`Has changed vote weight`) },
                     { condition: true, label: this.translate.instant(`Has unchanged vote weight`) }
                 ]
-            },
-            {
-                property: `id`,
-                label: this.translate.instant(`Meetings`),
-                options: meetingFilterOptions
-            },
-            this.committeeFilterOptions
+            }
         ];
         return staticFilterDefinitions;
     }
