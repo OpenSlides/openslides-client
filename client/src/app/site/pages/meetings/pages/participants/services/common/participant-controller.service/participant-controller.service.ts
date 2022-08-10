2d07f6d2c52e0d000d678ab63d3c8d6f52f7b8e7
Luisa <luisa.beerboom@intevation.de>
2022-08-10 11:23:59 +0200

    Non group-related meeting user updates no longer set group to standard

--- client/src/app/site/pages/meetings/pages/participants/services/common/participant-controller.service/participant-controller.service.ts
+++ client/src/app/site/pages/meetings/pages/participants/services/common/participant-controller.service/participant-controller.service.ts
@@ -232,7 +232,7 @@ export class ParticipantControllerService extends BaseMeetingControllerService<V
                 [this.activeMeetingId!]: participant.structure_level
             },
             group_$_ids: participant.group_$_ids || {
-                [this.activeMeetingId!]: participant.group_ids || [this.activeMeeting.meeting!.default_group_id]
+                [this.activeMeetingId!]: participant.group_ids
             },
             number_$: participant.number_$ || { [this.activeMeetingId!]: participant.number },
             vote_weight_$: participant.vote_weight_$ || {
