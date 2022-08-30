191264665d58559f3a1b5554bc829fcedeee6764
Luisa <luisa.beerboom@intevation.de>
2022-08-30 16:58:46 +0200

    Removal from speaker list now kicks user

--- client/src/app/site/pages/meetings/pages/interaction/services/interaction.service.ts
+++ client/src/app/site/pages/meetings/pages/interaction/services/interaction.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { marker as _ } from '@biesbjerg/ngx-translate-extract-marker';
-import { BehaviorSubject, combineLatest, distinctUntilChanged, filter, map, merge, Observable } from 'rxjs';
+import { TranslateService } from '@ngx-translate/core';
+import {
+    BehaviorSubject,
+    combineLatest,
+    distinctUntilChanged,
+    filter,
+    firstValueFrom,
+    map,
+    merge,
+    Observable
+} from 'rxjs';
 import { Id } from 'src/app/domain/definitions/key-types';
 import { OperatorService } from 'src/app/site/services/operator.service';
 import { PromptService } from 'src/app/ui/modules/prompt-dialog';
 
-import { NotifyService } from '../../../../../../gateways/notify.service';
+import { NotifyResponse, NotifyService } from '../../../../../../gateways/notify.service';
+import { ViewUser } from '../../../view-models/view-user';
 import { BroadcastService } from './broadcast.service';
 import { CallRestrictionService } from './call-restriction.service';
 import { InteractionServiceModule } from './interaction-service.module';
@@ -22,9 +34,14 @@ interface senderMessage {
     inviter: string;
 }
 
+interface kickMessage {
+    reason: string;
+}
+
 const BroadcastMessageType = `callInteraction`;
 const InviteMessage = `invitationToCall`;
 const CallInviteTitle = _(`Please join the conference room now!`);
+const KickMessage = `kickFromCall`;
 
 @Injectable({
     providedIn: InteractionServiceModule
@@ -52,6 +69,8 @@ export class InteractionService {
         return this.conferenceStateObservable.pipe(map(state => state === ConferenceState.none));
     }
 
+    private _kickObservable = this.notifyService.getMessageObservable<kickMessage>(KickMessage);
+
     public constructor(
         private streamService: StreamService,
         private rtcService: RtcService,
@@ -59,7 +78,9 @@ export class InteractionService {
         private notifyService: NotifyService,
         private operator: OperatorService,
         private promptService: PromptService,
-        private broadcast: BroadcastService
+        private broadcast: BroadcastService,
+        private snackBar: MatSnackBar,
+        private translate: TranslateService
     ) {
         combineLatest([
             this.showLiveConfObservable,
@@ -108,6 +129,8 @@ export class InteractionService {
         broadcast.get(BroadcastMessageType).subscribe(() => {
             this.promptService.close();
         });
+
+        this._kickObservable.subscribe(response => this.onKickMessage(response));
     }
 
     public async enterCall(): Promise<void> {
@@ -126,6 +149,20 @@ export class InteractionService {
     public viewStream(): void {
         if (this.conferenceState !== ConferenceState.stream) {
             this.setConferenceState(ConferenceState.stream);
+        }
+    }
+
+    public kickUsers(users: ViewUser[], reason?: string): void {
+        this.notifyService.sendToUsers(KickMessage, { reason }, ...users.map(user => user.id));
+    }
+
+    private async onKickMessage(message: NotifyResponse<kickMessage>): Promise<void> {
+        if (await firstValueFrom(this.rtcService.isJitsiActiveObservable)) {
+            this.rtcService.stopJitsi();
+            const snackBarMessage = `${this.translate.instant(
+                _(`Kicked out of conference call for the following reason`)
+            )}: ${this.translate.instant(message.message.reason)}.`;
+            this.snackBar.open(snackBarMessage, `Ok`);
         }
     }
 
