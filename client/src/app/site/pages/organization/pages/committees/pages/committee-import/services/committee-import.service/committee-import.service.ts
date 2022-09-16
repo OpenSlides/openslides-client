096c610644a402d905d05d6b79d3c3bb19c2e5ab
Luisa <luisa.beerboom@intevation.de>
2022-09-16 10:26:00 +0200

    cleanup

--- client/src/app/site/pages/organization/pages/committees/pages/committee-import/services/committee-import.service/committee-import.service.ts
+++ client/src/app/site/pages/organization/pages/committees/pages/committee-import/services/committee-import.service/committee-import.service.ts
@@ -165,9 +165,7 @@ export class CommitteeImportService extends BaseImportService<CommitteeCsvPort> 
                                 };
                             })
                             .filter(update => !!update.id);
-                        return (await this.repo
-                            .update(null, ...(updates as any))
-                            .resolve()) as unknown as Promise<void>;
+                        await this.repo.update(null, ...(updates as any)).resolve();
                     },
                     labelFn: (phase, plural) => {
                         const verboseName = plural ? _(`Forwardings`) : _(`Forwarding`);
@@ -289,7 +287,9 @@ export class CommitteeImportService extends BaseImportService<CommitteeCsvPort> 
                 this.repo.getViewModelList().filter(committee => committee.name === entry.name),
             shouldCreateModelFn: entry => entry.status === `new`,
             createFn: entries => this.repo.create(...(entries as any)),
-            updateFn: entries => this.repo.update(null, ...(entries as any)).resolve() as unknown as Promise<void>,
+            updateFn: async entries => {
+                await this.repo.update(null, ...(entries as any)).resolve();
+            },
             requiredFields: [NAME]
         };
     }
