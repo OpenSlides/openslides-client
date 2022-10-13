7d586491763948f69282ddac768fea7d17758122
Luisa <luisa.beerboom@intevation.de>
2022-10-13 10:20:16 +0200

    Participants: Reordered import and export columns

--- client/src/app/site/pages/meetings/pages/participants/export/participant-csv-export.service/participant-csv-export.service.ts
+++ client/src/app/site/pages/meetings/pages/participants/export/participant-csv-export.service/participant-csv-export.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { UserExport } from 'src/app/domain/models/users/user.export';
-import { CsvExportService } from 'src/app/gateways/export/csv-export.service';
+import {
+    CsvColumnDefinitionMap,
+    CsvColumnDefinitionProperty,
+    CsvColumnsDefinition,
+    CsvExportService
+} from 'src/app/gateways/export/csv-export.service';
 import { ViewUser } from 'src/app/site/pages/meetings/view-models/view-user';
 
 import { participantHeadersAndVerboseNames } from '../../pages/participant-import/definitions';
@@ -18,36 +23,35 @@ export interface ParticipantExport extends UserExport {
     providedIn: ParticipantExportModule
 })
 export class ParticipantCsvExportService {
+    private _csvColumnDefinitionMapsMap: Map<string, CsvColumnDefinitionMap<ViewUser>> = new Map([
+        [
+            `group_ids`,
+            {
+                label: `Groups`,
+                map: user =>
+                    user
+                        .groups()
+                        .map(group => group.name)
+                        .join(`,`)
+            }
+        ]
+    ]);
+
     public constructor(private csvExport: CsvExportService, private translate: TranslateService) {}
 
     public export(participants: ViewUser[]): void {
         this.csvExport.export(
             participants,
-            [
-                { property: `title` },
-                { property: `first_name`, label: `Given name` },
-                { property: `last_name`, label: `Surname` },
-                { property: `structure_level`, label: `Structure level` },
-                { property: `number`, label: `Participant number` },
-                {
-                    label: `groups`,
-                    map: user =>
-                        user
-                            .groups()
-                            .map(group => group.name)
-                            .join(`,`)
-                },
-                { property: `comment` },
-                { property: `is_active`, label: `Is active` },
-                { property: `is_present_in_meetings`, label: `Is present in meeting` },
-                { property: `is_physical_person`, label: `Is a natural person` },
-                { property: `default_password`, label: `Initial password` },
-                { property: `email` },
-                { property: `username` },
-                { property: `gender` },
-                { property: `vote_weight`, label: `Vote weight` },
-                { property: `pronoun`, label: `Pronoun` }
-            ],
+            Object.keys(participantHeadersAndVerboseNames).map(key => {
+                const map = this._csvColumnDefinitionMapsMap.get(key);
+                if (map) {
+                    return map;
+                }
+                return {
+                    property: key,
+                    label: participantHeadersAndVerboseNames[key]
+                } as CsvColumnDefinitionProperty<ViewUser>;
+            }) as CsvColumnsDefinition<ViewUser>,
             this.translate.instant(`Participants`) + `.csv`
         );
     }
