78af6d27ecdf4ba3088184c9fd61c2697989451d
Bastian Rihm <Bastian Rihm brihm@intevation.de>
2022-11-23 11:57:54 +0100

    remove fields from meeting participant list

--- client/src/app/site/pages/meetings/pages/participants/components/participant-main/participant-main.component.ts
+++ client/src/app/site/pages/meetings/pages/participants/components/participant-main/participant-main.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 import { BaseModelRequestHandlerComponent } from 'src/app/site/base/base-model-request-handler.component';
 
+import { getParticipantListSubscriptionConfig } from '../../config/model-subscription';
+
 @Component({
     selector: `os-participant-main`,
     templateUrl: `./participant-main.component.html`,
     styleUrls: [`./participant-main.component.scss`]
 })
-export class ParticipantMainComponent extends BaseModelRequestHandlerComponent {}
+export class ParticipantMainComponent extends BaseModelRequestHandlerComponent {
+    protected override onNextMeetingId(id: number | null): void {
+        if (id) {
+            this.subscribeTo(getParticipantListSubscriptionConfig(id, () => this.hasMeetingIdChangedObservable()));
+        }
+    }
+}
