95dfeacaafad3acd804661585e3227cb3443d791
Bastian Rihm <Bastian Rihm brihm@intevation.de>
2022-11-29 09:29:07 +0100

    reset changes to user repo

--- client/src/app/gateways/repositories/users/user-repository.service.ts
+++ client/src/app/gateways/repositories/users/user-repository.service.ts
@@ -102,27 +102,17 @@ export class UserRepositoryService extends BaseRepository<ViewUser, User> {
             { templateField: `committee_$_management_level` }
         ]);
 
-        const participantListFieldsMinimal: TypedFieldset<User> = [
-            `title`,
-            `first_name`,
-            `last_name`,
-            `pronoun`,
-            `username` /* Required! To getShortName */,
-            `gender`,
-            `meeting_ids`,
-            { templateField: `structure_level_$` },
-            { templateField: `number_$` },
-            { templateField: `group_$_ids` }
-        ];
-
-        const participantListFields: TypedFieldset<User> = shortNameFields.concat([
+        const participantListFieldsMinimal: TypedFieldset<User> = shortNameFields.concat([
             { templateField: `vote_delegated_$_to_id` },
             { templateField: `vote_delegations_$_from_ids` },
             { templateField: `vote_weight_$` },
             { templateField: `structure_level_$` },
             { templateField: `number_$` },
             { templateField: `comment_$` },
-            { templateField: `group_$_ids` },
+            { templateField: `group_$_ids` }
+        ]);
+
+        const participantListFields: TypedFieldset<User> = participantListFieldsMinimal.concat([
             `is_present_in_meeting_ids`
         ]);
 
