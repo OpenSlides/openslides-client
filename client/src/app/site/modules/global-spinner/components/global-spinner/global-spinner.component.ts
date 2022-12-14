3ca0b7c5c275f7b220086fdd28358f8cb7a3ac5b
Bastian Rihm <Bastian Rihm brihm@intevation.de>
2022-12-14 12:15:15 +0100

    add error to global spinner

---
+++ client/src/app/site/modules/global-spinner/components/global-spinner/global-spinner.component.ts
@@ -0,0 +1,55 @@
+import { Component, Input } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { ConnectionStatusService } from 'src/app/site/services/connection-status.service';
+import { LifecycleService } from 'src/app/site/services/lifecycle.service';
+import { SpinnerComponent } from 'src/app/ui/modules/spinner/components/spinner/spinner.component';
+
+@Component({
+    selector: `os-global-spinner`,
+    templateUrl: `./global-spinner.component.html`,
+    styleUrls: [`./global-spinner.component.scss`]
+})
+export class GlobalSpinnerComponent extends SpinnerComponent {
+    @Input()
+    public spinnerConfig: { text?: string };
+
+    public displayDetailedInformation = false;
+    public isOperatorReady = false;
+
+    public get spinnerText(): string {
+        let errorText = [];
+        if (this.displayDetailedInformation) {
+            if (this.connectionStatusService.isOffline()) {
+                errorText.push(
+                    this.translate.instant(
+                        `There seems to be a problem connecting to our services. Check your connection or try again later.`
+                    )
+                );
+                if (this.connectionStatusService.getReason()) {
+                    errorText.push(this.connectionStatusService.getReason());
+                }
+            }
+        }
+
+        return errorText.join(`\n`) || this.spinnerConfig?.text;
+    }
+
+    public constructor(
+        private lifecycleService: LifecycleService,
+        private connectionStatusService: ConnectionStatusService,
+        private translate: TranslateService
+    ) {
+        super();
+
+        this.lifecycleService.booted.then(() => {
+            setTimeout(() => {
+                this.displayDetailedInformation = true;
+                console.log(
+                    this.displayDetailedInformation,
+                    this.connectionStatusService.isOffline(),
+                    this.connectionStatusService.getReason()
+                );
+            }, 10000);
+        });
+    }
+}
--- client/src/app/site/modules/global-spinner/global-spinner.module.ts
+++ client/src/app/site/modules/global-spinner/global-spinner.module.ts
@@ -3,8 +3,11 @@ import { NgModule } from '@angular/core';
 import { OpenSlidesOverlayModule } from 'src/app/ui/modules/openslides-overlay';
 import { SpinnerModule } from 'src/app/ui/modules/spinner';
 
+import { OpenSlidesTranslationModule } from '../translations';
+import { GlobalSpinnerComponent } from './components/global-spinner/global-spinner.component';
+
 @NgModule({
-    declarations: [],
-    imports: [CommonModule, SpinnerModule, OpenSlidesOverlayModule]
+    declarations: [GlobalSpinnerComponent],
+    imports: [CommonModule, SpinnerModule, OpenSlidesTranslationModule.forChild(), OpenSlidesOverlayModule]
 })
 export class GlobalSpinnerModule {}
