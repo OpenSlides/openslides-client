febbb9e376c62a25d53002dd80a3a647a24a4aa7
Luisa <luisa.beerboom@intevation.de>
2023-01-06 15:12:05 +0100

    Fixed problems where template meetings were unwantedly  included in lists

--- client/src/app/site/pages/organization/pages/committees/pages/committee-detail/modules/committee-detail-edit/components/committee-detail-edit/committee-detail-edit.component.ts
+++ client/src/app/site/pages/organization/pages/committees/pages/committee-detail/modules/committee-detail-edit/components/committee-detail-edit/committee-detail-edit.component.ts
@@ -3,14 +3,12 @@ import { AbstractControl, UntypedFormBuilder, UntypedFormGroup, Validators } fro
 import { ActivatedRoute } from '@angular/router';
 import { marker as _ } from '@biesbjerg/ngx-translate-extract-marker';
 import { TranslateService } from '@ngx-translate/core';
-import { map, Observable, OperatorFunction } from 'rxjs';
+import { map, OperatorFunction } from 'rxjs';
 import { Id } from 'src/app/domain/definitions/key-types';
 import { CML } from 'src/app/domain/definitions/organization-permission';
 import { Identifiable } from 'src/app/domain/interfaces';
 import { Selectable } from 'src/app/domain/interfaces/selectable';
 import { BaseComponent } from 'src/app/site/base/base.component';
-import { MeetingControllerService } from 'src/app/site/pages/meetings/services/meeting-controller.service';
-import { ViewMeeting } from 'src/app/site/pages/meetings/view-models/view-meeting';
 import { ViewUser } from 'src/app/site/pages/meetings/view-models/view-user';
 import { OrganizationTagControllerService } from 'src/app/site/pages/organization/pages/organization-tags/services/organization-tag-controller.service';
 import { ComponentServiceCollectorService } from 'src/app/site/services/component-service-collector.service';
@@ -38,7 +36,6 @@ export class CommitteeDetailEditComponent extends BaseComponent implements OnIni
 
     public isCreateView: boolean = false;
     public committeeForm!: UntypedFormGroup;
-    public meetingsObservable: Observable<ViewMeeting[]>;
 
     public editCommittee!: ViewCommittee;
 
@@ -54,7 +51,6 @@ export class CommitteeDetailEditComponent extends BaseComponent implements OnIni
         private formBuilder: UntypedFormBuilder,
         public committeeRepo: CommitteeControllerService,
         public orgaTagRepo: OrganizationTagControllerService,
-        private meetingRepo: MeetingControllerService,
         private route: ActivatedRoute,
         private operator: OperatorService
     ) {
@@ -67,7 +63,6 @@ export class CommitteeDetailEditComponent extends BaseComponent implements OnIni
         } else {
             super.setTitle(EDIT_COMMITTEE_LABEL);
         }
-        this.meetingsObservable = this.meetingRepo.getViewModelListObservable();
     }
 
     public ngOnInit(): void {
