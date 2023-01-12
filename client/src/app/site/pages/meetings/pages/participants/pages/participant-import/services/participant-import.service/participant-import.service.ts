0ffb2872dab3f8b8008a19e95bf604c3f1ec5447
Bastian Rihm <Bastian Rihm brihm@intevation.de>
2023-01-12 12:16:24 +0100

    fix participant reference not working on import

--- client/src/app/site/pages/meetings/pages/participants/pages/participant-import/services/participant-import.service/participant-import.service.ts
+++ client/src/app/site/pages/meetings/pages/participants/pages/participant-import/services/participant-import.service/participant-import.service.ts
@@ -127,7 +127,8 @@ export class ParticipantImportService extends BaseUserImportService {
     }): Promise<ImportModel<User>> {
         const username = input.username ? input.username : `${input.first_name} ${input.last_name}`;
         const userEmailUsername = `${username}/${input.email}`;
-        const duplicates = this._existingUserMap[userEmailUsername] ?? [];
+        const userUsername = `${username}/`;
+        const duplicates = this._existingUserMap[userUsername] ?? this._existingUserMap[userEmailUsername] ?? [];
         const newEntry = duplicates.length === 1 ? { ...duplicates[0], ...input } : input;
         const hasDuplicates =
             duplicates.length > 1 ||
