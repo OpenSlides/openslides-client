13e212a5ab1d0f3e789ea1622636566bfbb53b89
Bastian Rihm <Bastian Rihm brihm@intevation.de>
2023-01-24 13:53:43 +0100

    chip select with multiple items

--- client/src/app/site/modules/chip-select/chip-select.module.ts
+++ client/src/app/site/modules/chip-select/chip-select.module.ts
@@ -4,10 +4,11 @@ import { MatChipsModule } from '@angular/material/chips';
 import { MatMenuModule } from '@angular/material/menu';
 
 import { ChipSelectComponent } from './components/chip-select/chip-select.component';
+import { ChipSelectChipComponent } from './components/chip-select-chip/chip-select-chip.component';
 
 @NgModule({
-    exports: [ChipSelectComponent],
-    declarations: [ChipSelectComponent],
+    exports: [ChipSelectComponent, ChipSelectChipComponent],
+    declarations: [ChipSelectComponent, ChipSelectChipComponent],
     imports: [CommonModule, MatMenuModule, MatChipsModule]
 })
 export class ChipSelectModule {}
--- client/src/app/site/modules/chip-select/components/chip-select/chip-select.component.ts
+++ client/src/app/site/modules/chip-select/components/chip-select/chip-select.component.ts
@@ -1,16 +1,28 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { AfterContentInit, Component, ContentChildren, Input, OnInit, QueryList } from '@angular/core';
+
+import { ChipSelectChipComponent } from '../chip-select-chip/chip-select-chip.component';
 
 @Component({
     selector: `os-chip-select`,
     templateUrl: `./chip-select.component.html`,
-    styleUrls: [`./chip-select.component.scss`],
-    changeDetection: ChangeDetectionStrategy.OnPush
+    styleUrls: [`./chip-select.component.scss`]
 })
-export class ChipSelectComponent implements OnInit {
+export class ChipSelectComponent implements OnInit, AfterContentInit {
     constructor() {}
 
     @Input()
     public canOpen: boolean;
 
+    @ContentChildren(ChipSelectChipComponent) inputChips: QueryList<ChipSelectChipComponent>;
+
+    public chips: ChipSelectChipComponent[] = [];
+
     ngOnInit(): void {}
+
+    ngAfterContentInit(): void {
+        this.chips = this.inputChips.toArray();
+        this.inputChips.changes.subscribe((chips: ChipSelectChipComponent[]) => {
+            this.chips = chips;
+        });
+    }
 }
