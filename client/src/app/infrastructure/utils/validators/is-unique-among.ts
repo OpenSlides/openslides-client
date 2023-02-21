1f57aa6b05bf6daee38f0d6b839f322a270eb360
Bastian Rihm <brihm@intevation.de>
2023-02-21 15:55:57 +0100

    fix can not create motion if motion without number exists (#2046)

--- client/src/app/infrastructure/utils/validators/is-unique-among.ts
+++ client/src/app/infrastructure/utils/validators/is-unique-among.ts
@@ -3,10 +3,15 @@ import { BehaviorSubject } from 'rxjs';
 
 export function isUniqueAmong<T>(
     preExistingValues: T[] | BehaviorSubject<T[]>,
-    isEqualFn: (a: T, b: T) => boolean = (a, b) => a === b
+    isEqualFn: (a: T, b: T) => boolean = (a, b) => a === b,
+    ignoredValues: T[] = []
 ): ValidatorFn {
     return (formControl: AbstractControl): { [key: string]: any } | null => {
         const a = formControl.value as T;
+        if (ignoredValues.includes(a)) {
+            return null;
+        }
+
         const bValues = Array.isArray(preExistingValues) ? preExistingValues : preExistingValues.value;
         if (bValues.length && bValues.some(b => isEqualFn(a, b))) {
             return { notUniqueError: true };
