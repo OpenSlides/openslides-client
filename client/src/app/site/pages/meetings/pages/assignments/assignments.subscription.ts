722355727ed5dd20405946382ed48a145d2a7cfe
Bastian Rihm <Bastian Rihm brihm@intevation.de>
2023-03-10 10:16:23 +0100

    fix assignments

--- client/src/app/site/pages/meetings/pages/assignments/assignments.subscription.ts
+++ client/src/app/site/pages/meetings/pages/assignments/assignments.subscription.ts
@@ -2,13 +2,18 @@ import { Observable } from 'rxjs';
 import { Id } from 'src/app/domain/definitions/key-types';
 import { ViewMeeting } from 'src/app/site/pages/meetings/view-models/view-meeting';
 
+import { pollModelRequest } from '../polls/polls.subscription';
+
 const ASSIGNMENT_LIST_SUBSCRIPTION = `assignment_list`;
 
 export const getAssignmentSubscriptionConfig = (id: Id, hasMeetingIdChangedObservable: () => Observable<boolean>) => ({
     modelRequest: {
         viewModelCtor: ViewMeeting,
         ids: [id],
-        follow: [`assignment_ids`, `assignment_candidate_ids`]
+        follow: [
+            { idField: `assignment_ids`, follow: [{ idField: `poll_ids`, ...pollModelRequest }] },
+            `assignment_candidate_ids`
+        ]
     },
     subscriptionName: ASSIGNMENT_LIST_SUBSCRIPTION,
     hideWhen: hasMeetingIdChangedObservable()
