f0b321fa88ccf3c5ec160e0029f890fb4d66fd30
Bastian Rihm <Bastian Rihm brihm@intevation.de>
2023-03-10 15:24:37 +0100

    fix media files search selector and agenda attachment

--- client/src/app/site/pages/meetings/pages/mediafiles/mediafiles.subscription.ts
+++ client/src/app/site/pages/meetings/pages/mediafiles/mediafiles.subscription.ts
@@ -3,7 +3,7 @@ import { Id } from 'src/app/domain/definitions/key-types';
 
 import { ViewMeeting } from '../../view-models/view-meeting';
 
-export const MEDIAFILES_SUBSCRIPTION = `mediafiles`;
+export const MEDIAFILES_SUBSCRIPTION = `mediafiles_list`;
 
 export const getMediafilesSubscriptionConfig = (id: Id, hasMeetingIdChangedObservable: () => Observable<boolean>) => ({
     modelRequest: {
@@ -13,4 +13,16 @@ export const getMediafilesSubscriptionConfig = (id: Id, hasMeetingIdChangedObser
     },
     subscriptionName: MEDIAFILES_SUBSCRIPTION,
     hideWhen: hasMeetingIdChangedObservable()
+});
+
+export const MEDIAFILES_LIST_MINIMAL_SUBSCRIPTION = `mediafiles_list_minimal`;
+
+export const getMediafilesListMinimalSubscriptionConfig = (id: Id) => ({
+    modelRequest: {
+        viewModelCtor: ViewMeeting,
+        ids: [id],
+        follow: [`mediafile_ids`]
+    },
+    subscriptionName: MEDIAFILES_LIST_MINIMAL_SUBSCRIPTION,
+    hideWhenDestryed: true
 });
