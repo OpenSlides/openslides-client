ffeb54e59b8c45224c18bcce3775b6f6d48c7f7f
Bastian Rihm <Bastian Rihm brihm@intevation.de>
2023-03-21 17:10:06 +0100

    improve SubscriptionConfigGenerator type

--- client/src/app/domain/interfaces/subscription-config.ts
+++ client/src/app/domain/interfaces/subscription-config.ts
@@ -1,9 +1,10 @@
 import { Id } from 'src/app/domain/definitions/key-types';
+import { BaseViewModel } from 'src/app/site/base/base-view-model';
 import { SimplifiedModelRequest } from 'src/app/site/services/model-request-builder';
 
-export interface SubscriptionConfig {
-    modelRequest: SimplifiedModelRequest;
+export interface SubscriptionConfig<M extends BaseViewModel> {
+    modelRequest: SimplifiedModelRequest<M>;
     subscriptionName: string;
 }
 
-export type SubscriptionConfigGenerator = (...ids: Id[]) => SubscriptionConfig;
+export type SubscriptionConfigGenerator<M extends BaseViewModel = any> = (...ids: Id[]) => SubscriptionConfig<M>;
