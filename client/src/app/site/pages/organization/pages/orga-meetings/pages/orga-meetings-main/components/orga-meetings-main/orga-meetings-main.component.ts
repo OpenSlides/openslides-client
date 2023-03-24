8210ce54a6754f890a135412d6cb0937a622bbfb
Bastian Rihm <Bastian Rihm brihm@intevation.de>
2023-03-24 15:21:00 +0100

    fix OrgaMeetingsMainComponent

--- client/src/app/site/pages/organization/pages/orga-meetings/pages/orga-meetings-main/components/orga-meetings-main/orga-meetings-main.component.ts
+++ client/src/app/site/pages/organization/pages/orga-meetings/pages/orga-meetings-main/components/orga-meetings-main/orga-meetings-main.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
-import {
-    BaseModelRequestHandlerComponent,
-    ModelRequestConfig
-} from 'src/app/site/base/base-model-request-handler.component';
-import { getMeetingListSubscriptionConfig } from 'src/app/site/pages/organization/config/model-subscription';
+import { BaseModelRequestHandlerComponent } from 'src/app/site/base/base-model-request-handler.component';
+import { getMeetingListSubscriptionConfig } from 'src/app/site/pages/organization/organization.subscription';
 
 @Component({
     selector: `os-orga-meetings-main`,
@@ -11,7 +8,7 @@ import { getMeetingListSubscriptionConfig } from 'src/app/site/pages/organizatio
     styleUrls: [`./orga-meetings-main.component.scss`]
 })
 export class OrgaMeetingsMainComponent extends BaseModelRequestHandlerComponent {
-    protected override onCreateModelRequests(): void | ModelRequestConfig[] {
-        return [getMeetingListSubscriptionConfig(() => this.getNextMeetingIdObservable())];
+    protected override onShouldCreateModelRequests(): void {
+        this.subscribeTo(getMeetingListSubscriptionConfig(), { hideWhenMeetingChanged: true });
     }
 }
