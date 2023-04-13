290dadd1abbade629017c3cef9a7cd802cb8b837
Bastian Rihm <brihm@intevation.de>
2023-04-13 11:35:20 +0200

    disable tinymce blockquoute (#2184)

---
+++ client/src/app/site/pages/meetings/pages/motions/pages/motion-detail/definitions/tinymce-config.ts
@@ -0,0 +1,45 @@
+import { marker as _ } from '@biesbjerg/ngx-translate-extract-marker';
+
+export const MotionTinyMceConfig = {
+    style_formats: [
+        {
+            title: _(`Headers`),
+            items: [
+                { title: _(`Header 1`), format: `h1` },
+                { title: _(`Header 2`), format: `h2` },
+                { title: _(`Header 3`), format: `h3` },
+                { title: _(`Header 4`), format: `h4` },
+                { title: _(`Header 5`), format: `h5` },
+                { title: _(`Header 6`), format: `h6` }
+            ]
+        },
+        {
+            title: _(`Inline`),
+            items: [
+                { title: _(`Bold`), icon: `bold`, format: `bold` },
+                { title: _(`Italic`), icon: `italic`, format: `italic` },
+                { title: _(`Underline`), icon: `underline`, format: `underline` },
+                { title: _(`Strikethrough`), format: `strikethrough` },
+                { title: _(`Superscript`), icon: `superscript`, format: `superscript` },
+                { title: _(`Subscript`), icon: `subscript`, format: `subscript` },
+                { title: _(`Code`), format: `code` }
+            ]
+        },
+        {
+            title: _(`Blocks`),
+            items: [
+                { title: _(`Paragraph`), format: `p` },
+                { title: _(`Div`), format: `div` },
+                { title: _(`Pre`), format: `pre` }
+            ]
+        },
+        {
+            title: _(`Alignment`),
+            items: [
+                { title: _(`Left`), format: `alignleft` },
+                { title: _(`Center`), format: `aligncenter` },
+                { title: _(`Right`), format: `alignright` }
+            ]
+        }
+    ]
+};
