044af4f98a0e6481b50e0b33e0dc3e9f6443c8c5
Luisa <luisa.beerboom@intevation.de>
2023-06-06 14:41:38 +0200

    cleanup 2

--- client/src/app/ui/pipes/localized-date-range/localized-date-range.pipe.spec.ts
+++ client/src/app/ui/pipes/localized-date-range/localized-date-range.pipe.spec.ts
@@ -1,8 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+
 import { LocalizedDateRangePipe } from './localized-date-range.pipe';
 
 xdescribe(`LocalizedDateRangePipe`, () => {
+    let pipe: LocalizedDateRangePipe;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({}).compileComponents();
+
+        pipe = TestBed.inject(LocalizedDateRangePipe);
+    });
+
     it(`create an instance`, () => {
-        const pipe = new LocalizedDateRangePipe();
         expect(pipe).toBeTruthy();
     });
 });
