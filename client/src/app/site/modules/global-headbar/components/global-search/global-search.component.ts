6be73cecc62546da21ef18097c99e2fdb49e7a14
Bastian Rihm <Bastian Rihm brihm@intevation.de>
2023-06-20 14:35:48 +0200

    Fix search modal dark mode

--- client/src/app/site/modules/global-headbar/components/global-search/global-search.component.ts
+++ client/src/app/site/modules/global-headbar/components/global-search/global-search.component.ts
@@ -1,6 +1,9 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { Observable } from 'rxjs';
 import { GlobalSearchEntry, GlobalSearchService } from 'src/app/site/services/global-search.service';
+import { ThemeService } from 'src/app/site/services/theme.service';
+
 @Component({
     selector: `os-global-search`,
     templateUrl: `./global-search.component.html`,
@@ -25,7 +28,12 @@ export class GlobalSearchComponent {
 
     public results: { [key: string]: GlobalSearchEntry[] } = {};
 
+    public get isDarkModeObservable(): Observable<boolean> {
+        return this.themeService.isDarkModeObservable;
+    }
+
     public constructor(
+        private themeService: ThemeService,
         private globalSearchService: GlobalSearchService,
         private formBuilder: FormBuilder,
         private cd: ChangeDetectorRef
