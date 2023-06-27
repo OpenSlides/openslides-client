7c15ea3c6c2374598cf3e1e7c6e8a59a3a8ef593
Loki Elble <lelble@intevation.de>
2023-06-27 15:18:53 +0200

    test to string pipe

--- client/src/app/ui/pipes/to-string/to-string.pipe.spec.ts
+++ client/src/app/ui/pipes/to-string/to-string.pipe.spec.ts
@@ -1,8 +1,46 @@
 import { ToStringPipe } from './to-string.pipe';
 
-xdescribe(`ToStringPipe`, () => {
-    it(`create an instance`, () => {
+fdescribe(`ToStringPipe`, () => {
+    it(`test number to string`, () => {
         const pipe = new ToStringPipe();
-        expect(pipe).toBeTruthy();
+        expect(pipe.transform(1)).toEqual(`1`);
+    });
+
+    it(`test input string`, () => {
+        const pipe = new ToStringPipe();
+        expect(pipe.transform(`a`)).toEqual(`a`);
+    });
+
+    it(`test with nothing`, () => {
+        const pipe = new ToStringPipe();
+        expect(pipe.transform()).toEqual(``);
+    });
+
+    it(`test with function`, () => {
+        const pipe = new ToStringPipe();
+        let func: () => number = function (): number {
+            return 10;
+        };
+        expect(pipe.transform(func)).toEqual(`10`);
+    });
+
+    it(`test array to string`, () => {
+        const pipe = new ToStringPipe();
+        const array = [`a`, `b`, `c`];
+        expect(pipe.transform(array, 1)).toEqual(`b`);
+    });
+
+    it(`test with object`, () => {
+        const pipe = new ToStringPipe();
+        const obj = {
+            str: `a`,
+            num: 1,
+            func: () => {
+                return 10;
+            }
+        };
+        expect(pipe.transform(obj, `str`)).toEqual(`a`);
+        expect(pipe.transform(obj, `num`)).toEqual(`1`);
+        expect(pipe.transform(obj, `func`)).toEqual(`10`);
     });
 });
