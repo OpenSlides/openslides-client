9e7ade242daca9384af9d8aad26ebe6965bc13f9
Luisa <luisa.beerboom@intevation.de>
2023-06-27 13:47:32 +0200

    Add PollKeyVerbosePipe test

--- client/src/app/site/pages/meetings/modules/poll/pipes/poll-key-verbose/poll-key-verbose.pipe.spec.ts
+++ client/src/app/site/pages/meetings/modules/poll/pipes/poll-key-verbose/poll-key-verbose.pipe.spec.ts
@@ -1,8 +1,37 @@
+import { TestBed } from '@angular/core/testing';
+
 import { PollKeyVerbosePipe } from './poll-key-verbose.pipe';
 
-xdescribe(`PollKeyVerbosePipe`, () => {
-    it(`create an instance`, () => {
-        const pipe = new PollKeyVerbosePipe();
-        expect(pipe).toBeTruthy();
+describe(`PollKeyVerbosePipe`, () => {
+    let pipe: PollKeyVerbosePipe;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            providers: [PollKeyVerbosePipe]
+        }).compileComponents();
+
+        pipe = TestBed.inject(PollKeyVerbosePipe);
+    });
+
+    it(`test with legitimate PollValues`, () => {
+        expect(pipe.transform(`votesvalid`)).toBe(`Valid votes`);
+        expect(pipe.transform(`votesinvalid`)).toBe(`Invalid votes`);
+        expect(pipe.transform(`votescast`)).toBe(`Total votes cast`);
+        expect(pipe.transform(`votesno`)).toBe(`Votes No`);
+        expect(pipe.transform(`votesabstain`)).toBe(`Votes abstain`);
+        expect(pipe.transform(`yes`)).toBe(`Yes`);
+        expect(pipe.transform(`no`)).toBe(`No`);
+        expect(pipe.transform(`abstain`)).toBe(`Abstain`);
+        expect(pipe.transform(`amount_global_yes`)).toBe(`General approval`);
+        expect(pipe.transform(`amount_global_no`)).toBe(`General rejection`);
+        expect(pipe.transform(`amount_global_abstain`)).toBe(`General abstain`);
+    });
+
+    it(`test with other values`, () => {
+        expect(pipe.transform(`born`)).toBe(`born`);
+        expect(pipe.transform(`mad`)).toBe(`mad`);
+        expect(pipe.transform(`bad`)).toBe(`bad`);
+        expect(pipe.transform(`sad`)).toBe(`sad`);
+        expect(pipe.transform(`AAAAAAAAAAAAAAAAAAAAAAA`)).toBe(`AAAAAAAAAAAAAAAAAAAAAAA`);
     });
 });
