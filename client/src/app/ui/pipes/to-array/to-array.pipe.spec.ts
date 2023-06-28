d124a72bb38a55ea8d1372bfe8dfbbe415a8de00
Elblinator <69210919+Elblinator@users.noreply.github.com>
2023-06-28 15:39:31 +0200

    Add pipe unit tests (#2467)
    * Adds tests for readableBytes, toString, toArray

--- client/src/app/ui/pipes/to-array/to-array.pipe.spec.ts
+++ client/src/app/ui/pipes/to-array/to-array.pipe.spec.ts
@@ -1,8 +1,23 @@
 import { ToArrayPipe } from './to-array.pipe';
 
-xdescribe(`ToArrayPipe`, () => {
-    it(`create an instance`, () => {
+describe(`ToArrayPipe`, () => {
+    it(`test values`, () => {
         const pipe = new ToArrayPipe();
-        expect(pipe).toBeTruthy();
+        expect(pipe.transform(`a`)).toEqual([`a`]);
+        expect(pipe.transform(1)).toEqual([1]);
+        expect(pipe.transform(true)).toEqual([true]);
+    });
+
+    it(`test with null and undefined`, () => {
+        const pipe = new ToArrayPipe();
+        expect(pipe.transform(null)).toEqual([]);
+        expect(pipe.transform(undefined)).toEqual([]);
+    });
+
+    it(`test with array`, () => {
+        const pipe = new ToArrayPipe();
+        expect(pipe.transform<number>([1, 2])).toEqual([1, 2]);
+        expect(pipe.transform<string>([`1`, `2`])).toEqual([`1`, `2`]);
+        expect(pipe.transform<boolean>([true, false])).toEqual([true, false]);
     });
 });
