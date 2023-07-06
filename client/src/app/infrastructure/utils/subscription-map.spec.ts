99cbade9a5fb1469add20da3080f30838d01517e
Loki Elble <lelble@intevation.de>
2023-07-06 09:30:52 +0200

    fixes

--- client/src/app/infrastructure/utils/subscription-map.spec.ts
+++ client/src/app/infrastructure/utils/subscription-map.spec.ts
@@ -1,53 +1,47 @@
 import { Observable, Subscription } from 'rxjs';
 import { SubscriptionMap } from 'src/app/infrastructure/utils/subscription-map';
 
-const sub = new SubscriptionMap();
-
 describe(`utils: subscription-map`, () => {
-    describe(`delete method`, () => {
-        it(`delete one subscription from two`, () => {
-            let subscription: Subscription = new Observable().subscribe();
-            let subscription2: Subscription = new Observable().subscribe();
-            sub.updateSubscription(`test`, subscription);
-            sub.updateSubscription(`test2`, subscription2);
-            sub.delete(`test`);
-            expect(subscription.closed).toBe(true);
-            expect(subscription2.closed).toBe(false);
-        });
+    let sub: SubscriptionMap;
+    beforeEach(() => {
+        sub = new SubscriptionMap();
+    });
+    it(`delete method: delete one subscription from two`, () => {
+        let subscription: Subscription = new Observable().subscribe();
+        let subscription2: Subscription = new Observable().subscribe();
+        sub.updateSubscription(`test`, subscription);
+        sub.updateSubscription(`test2`, subscription2);
+        sub.delete(`test`);
+        expect(subscription.closed).toBe(true);
+        expect(subscription2.closed).toBe(false);
     });
 
-    describe(`clear method`, () => {
-        it(`delete all subscriptions`, () => {
-            let subscription: Subscription = new Observable().subscribe();
-            let subscription2: Subscription = new Observable().subscribe();
-            sub.updateSubscription(`test`, subscription);
-            sub.updateSubscription(`test2`, subscription2);
-            sub.clear();
-            expect(subscription.closed).toBe(true);
-            expect(subscription2.closed).toBe(true);
-        });
+    it(`clear method: delete all subscriptions`, () => {
+        let subscription: Subscription = new Observable().subscribe();
+        let subscription2: Subscription = new Observable().subscribe();
+        sub.updateSubscription(`test`, subscription);
+        sub.updateSubscription(`test2`, subscription2);
+        sub.clear();
+        expect(subscription.closed).toBe(true);
+        expect(subscription2.closed).toBe(true);
     });
 
-    describe(`updateSubscription method`, () => {
-        it(`add one subscription`, () => {
-            let subscription: Subscription = new Observable().subscribe();
-            sub.updateSubscription(`test`, subscription);
-            expect(subscription.closed).toBe(false);
-            sub.clear();
-            expect(subscription.closed).toBe(true);
-        });
+    it(`updateSubscription method: add one subscription`, () => {
+        let subscription: Subscription = new Observable().subscribe();
+        sub.updateSubscription(`test`, subscription);
+        expect(subscription.closed).toBe(false);
+        sub.clear();
+        expect(subscription.closed).toBe(true);
     });
 
-    describe(`push method`, () => {
-        it(`add several subscriptions`, () => {
-            let subscription: Subscription = new Observable().subscribe();
-            let subscription2: Subscription = new Observable().subscribe();
-            sub.push(subscription2, subscription);
-            expect(subscription.closed).toBe(false);
-            expect(subscription2.closed).toBe(false);
-            sub.clear();
-            expect(subscription.closed).toBe(true);
-            expect(subscription2.closed).toBe(true);
-        });
+    it(`push method: add several subscriptions`, () => {
+        let subscription: Subscription = new Observable().subscribe();
+        let subscription2: Subscription = new Observable().subscribe();
+        sub.push(subscription2, subscription);
+        expect(subscription.closed).toBe(false);
+        expect(subscription2.closed).toBe(false);
+        sub.clear();
+        expect(subscription.closed).toBe(true);
+        expect(subscription2.closed).toBe(true);
     });
 });
