59cded8a63648451d823f92f1722de22ff8c6b26
Luisa <luisa.beerboom@intevation.de>
2023-07-18 12:26:31 +0200

    Add tests for ExportMeetingPresenterService

--- client/src/app/gateways/presenter/export-meeting-presenter.service.spec.ts
+++ client/src/app/gateways/presenter/export-meeting-presenter.service.spec.ts
@@ -1,16 +1,32 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ExportMeetingPresenterService } from './export-meeting-presenter.service';
+import { Presenter } from './presenter';
+import { PresenterService } from './presenter.service';
+import { MockPresenterService } from './presenter.service.spec';
 
-xdescribe(`ExportMeetingPresenterService`, () => {
+describe(`ExportMeetingPresenterService`, () => {
     let service: ExportMeetingPresenterService;
+    let presenter: MockPresenterService;
+
+    const testMeeting = { name: `Some meeting` };
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            providers: [ExportMeetingPresenterService, { provide: PresenterService, useClass: MockPresenterService }]
+        });
+
         service = TestBed.inject(ExportMeetingPresenterService);
+        presenter = TestBed.inject(PresenterService) as unknown as MockPresenterService;
+        presenter.returnValueFns.set(Presenter.EXPORT_MEETING, (data?: any) => {
+            if (!data || !(typeof data === `object`) || !Number.isInteger(data.meeting_id)) {
+                return { error: `data had wrong format` };
+            }
+            return { returnValue: { meeting: { id: data.meeting_id, ...testMeeting } } };
+        });
     });
 
-    it(`should be created`, () => {
-        expect(service).toBeTruthy();
+    it(`should correctly call export_meeting`, async () => {
+        expect(await service.call({ meeting_id: 2 })).toEqual({ meeting: { id: 2, ...testMeeting } });
     });
 });
