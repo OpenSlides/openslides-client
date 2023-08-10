bff342e14c947efeb11bcde4ebfef24afc95ea01
Luisa <luisa.beerboom@intevation.de>
2023-08-10 12:06:36 +0200

    Fix tag list in motion list

--- client/src/app/site/pages/meetings/pages/motions/pages/motion-list/motion-list.module.ts
+++ client/src/app/site/pages/meetings/pages/motions/pages/motion-list/motion-list.module.ts
@@ -11,6 +11,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { OpenSlidesTranslationModule } from 'src/app/site/modules/translations';
 import { MeetingsComponentCollectorModule } from 'src/app/site/pages/meetings/modules/meetings-component-collector';
 import { DirectivesModule } from 'src/app/ui/directives';
+import { CommaSeparatedListingModule } from 'src/app/ui/modules/comma-separated-listing';
 import { GridModule } from 'src/app/ui/modules/grid';
 import { HeadBarModule } from 'src/app/ui/modules/head-bar';
 import { IconContainerModule } from 'src/app/ui/modules/icon-container/icon-container.module';
@@ -28,6 +29,7 @@ import { MotionListRoutingModule } from './motion-list-routing.module';
     declarations: [MotionListComponent],
     imports: [
         CommonModule,
+        CommaSeparatedListingModule,
         MotionListRoutingModule,
         MotionsListServiceModule,
         MotionListInfoDialogModule,
