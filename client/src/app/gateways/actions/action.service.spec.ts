6cd5183c26c91d320aa855c047b299fa9e0edbbb
Luisa <luisa.beerboom@intevation.de>
2023-08-15 16:59:28 +0200

    Add new cases to action.service.spec.ts

--- client/src/app/gateways/actions/action.service.spec.ts
+++ client/src/app/gateways/actions/action.service.spec.ts
@@ -3,23 +3,38 @@ import { QueryParams } from 'src/app/infrastructure/definitions/http';
 
 import { HttpService } from '../http.service';
 import { ActionService } from './action.service';
-import { ActionResponse } from './action-utils';
+import { ActionError, ActionResponse } from './action-utils';
+
+const ACTION_URL = `/system/action/handle_request`;
+const ACTION_SEPARATELY_URL = `/system/action/handle_separately`;
 
 class MockHttpService {
     public lastPosts: { path: string; data: any; queryParams: QueryParams }[] = [];
+
+    public returnType: `response` | `error` | `nothing` | `too little` | `non-response` = `response`;
 
     public constructor() {}
 
     public async post<R>(path: string, data: any, queryParams: QueryParams): Promise<R> {
         this.lastPosts.push({ path, data, queryParams });
-        // if (
-        //     path !== `/system/autoupdate/history_information` ||
-        //     Object.keys(queryParams).length !== 1 ||
-        //     !queryParams[`fqid`]
-        // ) {
-        //     throw new Error(`Parameters for http post had incorrect data`);
-        // }
-        return this.wrapInActionResponse([[data]]) as unknown as R;
+        if (
+            ![ACTION_URL, ACTION_SEPARATELY_URL].includes(path) ||
+            data.some(date => !date[`action`] || !date[`data`].length)
+        ) {
+            throw new Error(`Parameters for http post had incorrect data: ${JSON.stringify(this.lastPosts[0])}`);
+        }
+        switch (this.returnType) {
+            case `response`:
+                return this.wrapInActionResponse([[data]]) as unknown as R;
+            case `error`:
+                return this.getActionError(data[0].action) as unknown as R;
+            case `too little`:
+                return this.wrapInActionResponse([]) as unknown as R;
+            case `non-response`:
+                return `I am a string` as unknown as R;
+            default:
+                return null;
+        }
     }
 
     private wrapInActionResponse<R>(data?: ((R | null)[] | null)[]): ActionResponse<R> {
@@ -29,10 +44,18 @@ class MockHttpService {
             results: data
         };
     }
+
+    private getActionError(action: string): ActionError {
+        return {
+            success: false,
+            message: `Action ${action} failed`,
+            error_index: 234
+        };
+    }
 }
 
 const exampleData = [
-    { title: `undefined`, data: undefined, expected: `` },
+    { title: `undefined`, data: undefined, expected: `null` },
     { title: `null`, data: null, expected: `null` },
     { title: `'0'`, data: 0, expected: `0` },
     { title: `positive integer`, data: 1, expected: `1` },
@@ -58,10 +81,10 @@ function getActionData(start: number, end: number = 0): any {
     return getExampleSlice(start, end).map(date => date.data);
 }
 
-function getActionExpect(actionName: string, start: number, end: number = 0): any {
-    return `[[{"action":"${actionName}","data":[${getExampleSlice(start, end)
+function getActionExpect(actionName: string, start: number, end: number = 0, wrap = 2): any {
+    return `${new Array(wrap).fill(`[`).join(``)}{"action":"${actionName}","data":[${getExampleSlice(start, end)
         .map(date => `${date.expected}`)
-        .join(`,`)}]}]]`;
+        .join(`,`)}]}${new Array(wrap).fill(`]`).join(``)}`;
 }
 
 fdescribe(`ActionService`, () => {
@@ -82,17 +105,48 @@ fdescribe(`ActionService`, () => {
             JSON.stringify(await service.sendRequests([{ action: `arbitrary.action`, data: getActionData(1, 2) }]))
         ).toEqual(getActionExpect(`arbitrary.action`, 1, 2));
         expect(
-            JSON.stringify(await service.sendRequests([{ action: `arbitrary.action`, data: getActionData(1, 3) }]))
-        ).toEqual(getActionExpect(`arbitrary.action`, 1, 3));
+            JSON.stringify(await service.sendRequests([{ action: `another.action`, data: getActionData(0, 3) }]))
+        ).toEqual(getActionExpect(`another.action`, 0, 3));
+        expect(http.lastPosts.every(post => post.path === ACTION_URL) && http.lastPosts.length === 2).toBe(true);
     });
 
-    // it(`test sendRequests with handle_separately`, () => {
-    //     service.sendRequests([{ action: `arbitrary.action`, data: [] }], true);
-    // });
+    it(`test sendRequests with handle_separately`, async () => {
+        expect(
+            JSON.stringify(await service.sendRequests([{ action: `very.arbitrary`, data: getActionData(3, 6) }], true))
+        ).toEqual(getActionExpect(`very.arbitrary`, 3, 6));
+        expect(http.lastPosts.length === 1 && http.lastPosts[0].path === ACTION_SEPARATELY_URL).toBe(true);
+    });
 
-    // it(`test create`, () => {});
+    it(`test sendRequests errors`, async () => {
+        await expectAsync(service.sendRequests([])).toBeRejectedWithError(
+            `The action service did not return responses for each request.`
+        );
+        const actionPayload = [{ action: `majestically`, data: getActionData(6, 9) }];
+        http.returnType = `error`;
+        await expectAsync(service.sendRequests(actionPayload)).toBeRejectedWith(`Action majestically failed`);
+        http.returnType = `too little`;
+        await expectAsync(service.sendRequests(actionPayload)).toBeRejectedWithError(
+            `The action service did not return responses for each request.`
+        );
+        http.returnType = `non-response`;
+        await expectAsync(service.sendRequests(actionPayload)).toBeRejectedWithError(
+            `Unknown return type from action service`
+        );
+        http.returnType = `nothing`;
+        await expectAsync(service.sendRequests(actionPayload)).toBeResolvedTo(null);
+    });
 
-    // it(`test createFromArray`, () => {});
+    it(`test create`, async () => {
+        let action = service.create<any>({ action: `arbitrary.action`, data: getActionData(1, 2) });
+        expect(JSON.stringify(await action.resolve()) as string).toEqual(getActionExpect(`arbitrary.action`, 1, 2, 1));
+        expect(http.lastPosts.length === 1 && http.lastPosts[0].path === ACTION_URL).toBe(true);
+    });
 
-    // it(`test beforeActionFunction`, () => {});
+    it(`test createFromArray`, async () => {
+        let action = service.createFromArray<any>([{ action: `arbitrary.action`, data: getActionData(1, 2) }]);
+        expect(JSON.stringify(await action.resolve()) as string).toEqual(getActionExpect(`arbitrary.action`, 1, 2, 1));
+        expect(http.lastPosts.length === 1 && http.lastPosts[0].path === ACTION_URL).toBe(true);
+    });
 });
+
+// it(`test beforeActionFunction`, async () => {});
