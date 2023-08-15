7dea0c387ede96325c89a338f6a20606f353205b
luisa-beerboom <101706784+luisa-beerboom@users.noreply.github.com>
2023-08-15 11:17:12 +0200

    Fix rtf tests (#2638)

--- client/src/app/ui/modules/import-list/components/via-backend-import-list/backend-import-list.component.spec.ts
+++ client/src/app/ui/modules/import-list/components/via-backend-import-list/backend-import-list.component.spec.ts
@@ -1,10 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Identifiable } from 'src/app/domain/interfaces';
 
 import { BackendImportListComponent } from './backend-import-list.component';
 
 xdescribe(`ViaBackendImportListComponent`, () => {
-    let component: BackendImportListComponent;
-    let fixture: ComponentFixture<BackendImportListComponent>;
+    let component: BackendImportListComponent<Identifiable>;
+    let fixture: ComponentFixture<BackendImportListComponent<Identifiable>>;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
