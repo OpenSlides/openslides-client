25bbb2600761a0ebb6b156ecc97d804671a897b1
Elblinator <lelble@intevation.de>
2023-08-17 13:54:44 +0200

    adjustment

--- client/src/app/site/pages/meetings/pages/agenda/modules/topics/definitions/topics.constants.ts
+++ client/src/app/site/pages/meetings/pages/agenda/modules/topics/definitions/topics.constants.ts
@@ -1,9 +1,7 @@
-import { marker as _ } from '@biesbjerg/ngx-translate-extract-marker';
-
 export const topicHeadersAndVerboseNames = {
-    title: _(`Title`),
-    text: _(`Text`),
-    agenda_duration: _(`Duration`),
-    agenda_comment: _(`Comment`),
-    agenda_type: _(`Internal item`)
+    title: `title`,
+    text: `text`,
+    agenda_duration: `agenda_duration`,
+    agenda_comment: `agenda_comment`,
+    agenda_type: `agenda_type`
 };
