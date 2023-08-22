b0e640668d8037d48806826809c8f37c276e32b4
Luisa <luisa.beerboom@intevation.de>
2023-08-22 13:45:12 +0200

    subscribe to meeting and committee names in account lists

--- client/src/app/site/pages/organization/pages/accounts/pages/account-detail/components/account-detail-main/account-detail-main.component.ts
+++ client/src/app/site/pages/organization/pages/accounts/pages/account-detail/components/account-detail-main/account-detail-main.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { filter, firstValueFrom, map } from 'rxjs';
 import { BaseModelRequestHandlerComponent } from 'src/app/site/base/base-model-request-handler.component';
-import { getMeetingListSubscriptionConfig } from 'src/app/site/pages/organization/organization.subscription';
 
 import { getAccountDetailSubscriptionConfig } from '../../../../accounts.subscription';
 
@@ -11,18 +9,6 @@ import { getAccountDetailSubscriptionConfig } from '../../../../accounts.subscri
     styleUrls: [`./account-detail-main.component.scss`]
 })
 export class AccountDetailMainComponent extends BaseModelRequestHandlerComponent {
-    public constructor() {
-        super();
-        firstValueFrom(
-            this.router.events.pipe(
-                map(() => this.router.url.includes(`meetings`)),
-                filter(isAddToMeetings => isAddToMeetings)
-            )
-        ).then(() => {
-            this.subscribeTo(getMeetingListSubscriptionConfig());
-        });
-    }
-
     protected override onParamsChanged(params: any, oldParams: any): void {
         if (params[`id`] !== oldParams[`id`]) {
             const id = +params[`id`];
