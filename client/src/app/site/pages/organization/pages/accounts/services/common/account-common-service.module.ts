ca8b5361c87ddc37ecdc782010b9ec685e255be8
Luisa <luisa.beerboom@intevation.de>
2023-09-06 10:56:57 +0200

    Fix account import NullInjectorError

--- client/src/app/site/pages/organization/pages/accounts/services/common/account-common-service.module.ts
+++ client/src/app/site/pages/organization/pages/accounts/services/common/account-common-service.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { UserComponentsModule } from 'src/app/site/modules/user-components';
+import { PromptDialogModule } from 'src/app/ui/modules/prompt-dialog';
 
-@NgModule({ imports: [UserComponentsModule] })
+@NgModule({ imports: [UserComponentsModule, PromptDialogModule] })
 export class AccountCommonServiceModule {}
