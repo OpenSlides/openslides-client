4fd5579adfc8c9f8b1295dcb56dd8d13a204ee32
Ludwig Reiter <ludwig.reiter@intevation.de>
2023-10-25 14:44:45 +0200

    Change duration of request of speakers to hours:mins
    Update duration service.

--- client/src/app/site/services/duration.service.ts
+++ client/src/app/site/services/duration.service.ts
@@ -79,7 +79,7 @@ export class DurationService {
         const minutes = `0${Math.floor((duration % 3600) / 60)}`.slice(-2);
         const seconds = `0${Math.floor(duration % 60)}`.slice(-2);
         if (!isNaN(+minutes) && !isNaN(+seconds)) {
-            return `${prefix}${hours}:${minutes}:${seconds} h`;
+            return `${prefix}${hours}:${minutes} h`;
         } else {
             return ``;
         }
