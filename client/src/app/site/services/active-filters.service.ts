2c0b95f40f99f242b8d25ce9a5637ecedad720f4
reiterl <ludwig.reiter@intevation.de>
2023-10-27 10:46:43 +0200

    Update sort and filter at meeting switch (#2947)

--- client/src/app/site/services/active-filters.service.ts
+++ client/src/app/site/services/active-filters.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { StorageService } from 'src/app/gateways/storage.service';
+import { ActiveMeetingIdService } from 'src/app/site/pages/meetings/services/active-meeting-id.service';
 import { ActiveFiltersStoreService } from 'src/app/ui/modules/list/definitions';
 
 import { OsFilter } from '../base/base-filter.service';
@@ -8,13 +9,21 @@ import { OsFilter } from '../base/base-filter.service';
     providedIn: `root`
 })
 export class ActiveFiltersService implements ActiveFiltersStoreService {
-    public constructor(private store: StorageService) {}
+    public constructor(private store: StorageService, private activeMeetingIdService: ActiveMeetingIdService) {}
 
     public async save<V>(storageKey: string, filterDefinitions: OsFilter<V>[]): Promise<void> {
-        return await this.store.set(`filter_` + storageKey, filterDefinitions);
+        return await this.store.set(this.calcStorageKey(storageKey), filterDefinitions);
     }
 
     public load<V>(storageKey: string): Promise<OsFilter<V>[]> {
-        return this.store.get<OsFilter<V>[]>(`filter_` + storageKey);
+        return this.store.get<OsFilter<V>[]>(this.calcStorageKey(storageKey));
+    }
+
+    private calcStorageKey(storageKey: string): string {
+        const possibleMeetingId = this.activeMeetingIdService.meetingId;
+        if (possibleMeetingId) {
+            return `filter_${storageKey}_${possibleMeetingId}`;
+        }
+        return `filter_${storageKey}`;
     }
 }
