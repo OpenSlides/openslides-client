179e440c7d8fe4e37b59ed1bc7c9a8084e37e44f
Ludwig Reiter <ludwig.reiter@intevation.de>
2023-11-17 10:51:12 +0100

    Add tests for view port service

--- client/src/app/site/services/view-port.service.spec.ts
+++ client/src/app/site/services/view-port.service.spec.ts
@@ -1,16 +1,29 @@
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 import { TestBed } from '@angular/core/testing';
+import { BehaviorSubject } from 'rxjs';
 
 import { ViewPortService } from './view-port.service';
 
-xdescribe(`ViewPortService`, () => {
+class MockBreakpointObserver {
+    observe = _arr => new BehaviorSubject<BreakpointState>({ matches: true, breakpoints: {} });
+}
+
+describe(`ViewPortService`, () => {
     let service: ViewPortService;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            providers: [ViewPortService, { provide: BreakpointObserver, useClass: MockBreakpointObserver }]
+        });
         service = TestBed.inject(ViewPortService);
     });
 
-    it(`should be created`, () => {
-        expect(service).toBeTruthy();
+    it(`check if isMobile is set by checkForChange`, () => {
+        expect(service.isMobile).toBe(true);
+    });
+
+    it(`check if isMobileSubject can be used`, () => {
+        service.isMobileSubject.next(false);
+        expect(service.isMobile).toBe(false);
     });
 });
