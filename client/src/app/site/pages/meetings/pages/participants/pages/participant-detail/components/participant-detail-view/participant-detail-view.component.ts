b4727c14f91d61bf7fd95f3658c1fb04d4c0d1ee
Elblinator <lelble@intevation.de>
2023-11-30 11:15:38 +0100

    participant-mask disable save-button

--- client/src/app/site/pages/meetings/pages/participants/pages/participant-detail/components/participant-detail-view/participant-detail-view.component.ts
+++ client/src/app/site/pages/meetings/pages/participants/pages/participant-detail/components/participant-detail-view/participant-detail-view.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Validators } from '@angular/forms';
 import { marker as _ } from '@colsen1991/ngx-translate-extract-marker';
 import { TranslateService } from '@ngx-translate/core';
 import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
@@ -36,7 +37,8 @@ export class ParticipantDetailViewComponent extends BaseMeetingComponent {
     public participantSubscriptionConfig = getParticipantMinimalSubscriptionConfig(this.activeMeetingId);
 
     public readonly additionalFormControls = MEETING_RELATED_FORM_CONTROLS.mapToObject(controlName => ({
-        [controlName]: [``]
+        [controlName]: [``],
+        vote_weight: [``, Validators.min(0.000001)],
     }));
 
     public get randomPasswordFn(): () => string {
@@ -120,6 +122,10 @@ export class ParticipantDetailViewComponent extends BaseMeetingComponent {
         return this._isVoteDelegationEnabled;
     }
 
+    public get isDefaultVoteWeightError(): boolean {
+        return this.personalInfoFormValue.vote_weight < 0.000001;
+    }
+    
     private _userId: Id | undefined = undefined; // Not initialized
     private _isVoteWeightEnabled = false;
     private _isVoteDelegationEnabled = false;
