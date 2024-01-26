caef1fbf6a8b6cd9eff1591a650c1bd835e3bdf0
Bastian Rihm <brihm@intevation.de>
2024-01-26 16:07:22 +0100

    Speaker export add columns and filter (#3214)

--- client/src/app/site/pages/meetings/pages/participants/export/speaker-csv-export.service/speaker-csv-export.service.ts
+++ client/src/app/site/pages/meetings/pages/participants/export/speaker-csv-export.service/speaker-csv-export.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@angular/core';
 import { marker as _ } from '@colsen1991/ngx-translate-extract-marker';
 import { TranslateService } from '@ngx-translate/core';
-import { CsvColumnDefinitionProperty, CsvColumnsDefinition } from 'src/app/gateways/export/csv-export.service';
+import { format, fromUnixTime } from 'date-fns';
+import { DateFnsConfigurationService } from 'ngx-date-fns';
+import {
+    CsvColumnDefinitionMap,
+    CsvColumnDefinitionProperty,
+    CsvColumnsDefinition
+} from 'src/app/gateways/export/csv-export.service';
+import { DurationService } from 'src/app/site/services/duration.service';
 
 import { MeetingCsvExportForBackendService } from '../../../../services/export/meeting-csv-export-for-backend.service';
 import { ViewSpeaker } from '../../../agenda';
@@ -14,7 +21,8 @@ export const speakerHeadersAndVerboseNames = {
     structureLevelName: _(`Structure level`),
     user_number: _(`Participant number`),
     speech_state: _(`Speech type`),
-    speakingTime: _(`Time`),
+    begin_time: _(`Start time`),
+    speakingTime: _(`Duration`),
     topic: _(`Element`)
 };
 
@@ -22,12 +30,34 @@ export const speakerHeadersAndVerboseNames = {
     providedIn: ParticipantExportModule
 })
 export class SpeakerCsvExportService {
-    public constructor(private csvExport: MeetingCsvExportForBackendService, private translate: TranslateService) {}
+    public constructor(
+        private csvExport: MeetingCsvExportForBackendService,
+        private translate: TranslateService,
+        private durationService: DurationService,
+        private dfnsConfig: DateFnsConfigurationService
+    ) {}
+
+    private columnMappings = {
+        begin_time: (speaker: ViewSpeaker): string =>
+            speaker.begin_time
+                ? format(fromUnixTime(speaker.begin_time), `Ppp`, {
+                      locale: this.dfnsConfig.locale()
+                  })
+                : ``,
+        speakingTime: (speaker: ViewSpeaker): string => this.durationService.durationToString(speaker.speakingTime, ``)
+    };
 
     public export(speakers: ViewSpeaker[]): void {
         this.csvExport.export(
             speakers,
             Object.keys(speakerHeadersAndVerboseNames).map(key => {
+                if (this.columnMappings[key]) {
+                    return {
+                        map: speaker => this.columnMappings[key](speaker),
+                        label: this.translate.instant(speakerHeadersAndVerboseNames[key])
+                    } as CsvColumnDefinitionMap<ViewSpeaker>;
+                }
+
                 return {
                     property: key,
                     label: this.translate.instant(speakerHeadersAndVerboseNames[key])
--- client/src/app/site/pages/meetings/pages/participants/pages/participant-speaker-list/services/participant-speaker-list-filter.service/participant-speaker-list-filter.service.ts
+++ client/src/app/site/pages/meetings/pages/participants/pages/participant-speaker-list/services/participant-speaker-list-filter.service/participant-speaker-list-filter.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { SpeechState } from 'src/app/domain/models/speakers/speech-state';
 import { GENDER_FITLERABLE, GENDERS } from 'src/app/domain/models/users/user';
 import { OsFilter, OsHideFilterSetting } from 'src/app/site/base/base-filter.service';
 import { BaseMeetingFilterListService } from 'src/app/site/pages/meetings/base/base-meeting-filter-list.service';
@@ -51,6 +52,17 @@ export class ParticipantSpeakerListFilterService extends BaseMeetingFilterListSe
                     { condition: GENDER_FITLERABLE[2], label: this.translate.instant(GENDERS[2]) },
                     { condition: GENDER_FITLERABLE[3], label: this.translate.instant(GENDERS[3]) },
                     { condition: null, label: this.translate.instant(`unknown`) }
+                ]
+            },
+            {
+                property: `speech_state`,
+                label: this.translate.instant(`Type`),
+                options: [
+                    { condition: SpeechState.PRO, label: this.translate.instant(`Forspeech`) },
+                    { condition: SpeechState.CONTRA, label: this.translate.instant(`Contra speech`) },
+                    { condition: SpeechState.CONTRIBUTION, label: this.translate.instant(`Contribution`) },
+                    { condition: SpeechState.INTERVENTION, label: this.translate.instant(`Intervention`) },
+                    { condition: SpeechState.INTERPOSED_QUESTION, label: this.translate.instant(`Interposed question`) }
                 ]
             }
         ];
