fc00c2085142774bd3c1d5cc937b2d78370bb239
luisa-beerboom <101706784+luisa-beerboom@users.noreply.github.com>
2024-02-06 16:50:08 +0100

    Fix the glaring vote decrypt rebase bugs (#3246)

--- client/src/app/site/pages/meetings/modules/poll/components/check-vote-validity-dialog/check-vote-validity-dialog.component.ts
+++ client/src/app/site/pages/meetings/modules/poll/components/check-vote-validity-dialog/check-vote-validity-dialog.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA } from '@angular/material/legacy-dialog';
 import { VoteValuesVerbose } from 'src/app/domain/models/poll';
 
 import { ViewPoll } from '../../../../pages/polls';
