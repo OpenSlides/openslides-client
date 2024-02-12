6ef395771159466c8310e7852b478acf114fbba9
Joshua Sangmeister <joshua.sangmeister@gmail.com>
2024-02-12 14:44:50 +0100

    Fix karma test

--- client/src/app/site/pages/meetings/pages/motions/pages/motion-detail/components/motion-manage-motion-meeting-users/motion-manage-motion-meeting-users.component.spec.ts
+++ client/src/app/site/pages/meetings/pages/motions/pages/motion-detail/components/motion-manage-motion-meeting-users/motion-manage-motion-meeting-users.component.spec.ts
@@ -1,10 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MotionManageMotionMeetingUsersComponent } from './motion-manage-motion-meeting-users.component';
+import { BaseHasMeetingUserViewModel } from 'src/app/site/pages/meetings/base/base-has-meeting-user-view-model';
+import { BaseModel } from 'src/app/domain/models/base/base-model';
 
 xdescribe(`MotionManageSubmittersComponent`, () => {
-    let component: MotionManageMotionMeetingUsersComponent;
-    let fixture: ComponentFixture<MotionManageMotionMeetingUsersComponent>;
+    let component: MotionManageMotionMeetingUsersComponent<BaseHasMeetingUserViewModel, BaseModel>;
+    let fixture: ComponentFixture<MotionManageMotionMeetingUsersComponent<BaseHasMeetingUserViewModel, BaseModel>>;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
