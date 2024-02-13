032bc0a23be93524753c2ad6672dfe0fd0c9c48c
reiterl <ludwig.reiter@intevation.de>
2024-02-13 14:04:22 +0100

    Use backend import for committee import (#3260)

--- client/src/app/ui/modules/import-list/definitions/import-list-header-definition.ts
+++ client/src/app/ui/modules/import-list/definitions/import-list-header-definition.ts
@@ -5,7 +5,7 @@ export type ImportListHeaderDefinition = HeaderDefinition;
 interface HeaderDefinition {
     label: string;
     property: string;
-    type?: `boolean` | `string` | `number`;
+    type?: `boolean` | `string` | `number` | `date`;
     isRequired?: boolean;
     isTableColumn?: boolean;
     width?: number;
