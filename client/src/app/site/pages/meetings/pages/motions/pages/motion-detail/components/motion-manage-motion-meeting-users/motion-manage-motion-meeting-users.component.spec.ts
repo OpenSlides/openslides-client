fba5cbfb2801458992c6700271dbafbf040c7bd1
Joshua Sangmeister <joshua.sangmeister@gmail.com>
2024-02-14 11:58:19 +0100

    Fix linting

--- client/src/app/site/pages/meetings/pages/motions/pages/motion-detail/components/motion-manage-motion-meeting-users/motion-manage-motion-meeting-users.component.spec.ts
+++ client/src/app/site/pages/meetings/pages/motions/pages/motion-detail/components/motion-manage-motion-meeting-users/motion-manage-motion-meeting-users.component.spec.ts
@@ -1,8 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BaseModel } from 'src/app/domain/models/base/base-model';
+import { BaseHasMeetingUserViewModel } from 'src/app/site/pages/meetings/base/base-has-meeting-user-view-model';
 
 import { MotionManageMotionMeetingUsersComponent } from './motion-manage-motion-meeting-users.component';
-import { BaseHasMeetingUserViewModel } from 'src/app/site/pages/meetings/base/base-has-meeting-user-view-model';
-import { BaseModel } from 'src/app/domain/models/base/base-model';
 
 xdescribe(`MotionManageSubmittersComponent`, () => {
     let component: MotionManageMotionMeetingUsersComponent<BaseHasMeetingUserViewModel, BaseModel>;
