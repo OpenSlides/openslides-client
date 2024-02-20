dde8d4057b6dc38d0df33efba5d08b172484f074
Joshua Sangmeister <joshua.sangmeister@gmail.com>
2024-02-20 14:31:35 +0100

    Fix new motion field filters

--- client/src/app/site/pages/meetings/pages/motions/view-models/view-motion.ts
+++ client/src/app/site/pages/meetings/pages/motions/view-models/view-motion.ts
@@ -74,6 +74,14 @@ export class ViewMotion extends BaseProjectableViewModel<Motion> {
         return (this.submitters || []).map(submitter => submitter.user);
     }
 
+    public get editorUserIds(): Id[] {
+        return (this.editors || []).map(editor => editor.user_id);
+    }
+
+    public get workingGroupSpeakerUserIds(): Id[] {
+        return (this.working_group_speakers || []).map(workingGroupSpeaker => workingGroupSpeaker.user_id);
+    }
+
     public get numberOrTitle(): string {
         return this.number ? this.number : this.title;
     }
