84a7a767c90be2a2b8f4015830694bbe999624aa
rrenkert <raimund.renkert@intevation.de>
2024-02-21 16:24:00 +0100

    Fixed linting

--- client/src/app/site/pages/meetings/modules/list-of-speakers-content/components/list-of-speakers-content/list-of-speakers-content.component.ts
+++ client/src/app/site/pages/meetings/modules/list-of-speakers-content/components/list-of-speakers-content/list-of-speakers-content.component.ts
@@ -660,8 +660,8 @@ export class ListOfSpeakersContentComponent extends BaseMeetingComponent impleme
     }
 
     private findOperatorSpeaker(pointOfOrder?: boolean): ViewSpeaker | undefined {
-        return this.waitingSpeakers.sort((a, b) => b.id - a.id).find(
-            speaker => speaker.user_id === this.operator.operatorId && speaker.point_of_order === pointOfOrder
-        );
+        return this.waitingSpeakers
+            .sort((a, b) => b.id - a.id)
+            .find(speaker => speaker.user_id === this.operator.operatorId && speaker.point_of_order === pointOfOrder);
     }
 }
