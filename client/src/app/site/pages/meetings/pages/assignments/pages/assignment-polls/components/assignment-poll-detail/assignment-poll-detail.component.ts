903ef0ad1b06a1e2941ec0b6ee8676ee0769b0c0
Luisa <luisa.beerboom@intevation.de>
2024-02-29 15:34:18 +0100

    Ensure that general abstain is listed in single votes

--- client/src/app/site/pages/meetings/pages/assignments/pages/assignment-polls/components/assignment-poll-detail/assignment-poll-detail.component.ts
+++ client/src/app/site/pages/meetings/pages/assignments/pages/assignment-polls/components/assignment-poll-detail/assignment-poll-detail.component.ts
@@ -86,7 +86,7 @@ export class AssignmentPollDetailComponent
                 }
             }
         }
-        for (const vote of this.poll.global_option.votes) {
+        for (const vote of this.poll.global_option?.votes ?? []) {
             if (vote.weight > 0) {
                 // global vote must be the only vote, so we can just ignore any previous value
                 votes[vote.user_token] = {
