0ebe13ccf771526857b7392212daeadfeda7833f
Emanuel Schütze <emanuel@intevation.de>
2024-03-08 14:31:18 +0100

    Fix linter error

--- client/src/app/gateways/repositories/motions/motion-working-group-speaker-repository/motion-working-group-speaker-repository.service.ts
+++ client/src/app/gateways/repositories/motions/motion-working-group-speaker-repository/motion-working-group-speaker-repository.service.ts
@@ -19,6 +19,5 @@ export class MotionWorkingGroupSpeakerRepositoryService extends BaseMotionMeetin
         super(repositoryServiceCollector, MotionWorkingGroupSpeaker, MotionWorkingGroupSpeakerAction);
     }
 
-    public getVerboseName = (plural = false) =>
-        this.translate.instant(plural ? `Spokespersons` : `Spokesperson`);
+    public getVerboseName = (plural = false) => this.translate.instant(plural ? `Spokespersons` : `Spokesperson`);
 }
