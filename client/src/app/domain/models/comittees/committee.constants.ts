7ea5d15b1d99e0ec79720cb4eaebe84e803462ac
Elblinator <lelble@intevation.de>
2024-03-26 12:29:50 +0100

    remove exports

--- client/src/app/domain/models/comittees/committee.constants.ts
+++ client/src/app/domain/models/comittees/committee.constants.ts
@@ -1,18 +1,16 @@
-import { marker as _ } from '@colsen1991/ngx-translate-extract-marker';
-
 import { Id } from '../../definitions/key-types';
 
-export const ID = `id`;
-export const NAME = `name`;
-export const DESCRIPTION = `description`;
-export const FORWARD_TO_COMMITTEES = `forward_to_committees`;
-export const ORGANIZATION_TAGS = `organization_tags`;
-export const MANAGERS = `managers`;
-export const MEETING_NAME = `meeting_name`;
-export const MEETING_START_TIME = `meeting_start_time`;
-export const MEETING_END_TIME = `meeting_end_time`;
-export const MEETING_ADMINS = `meeting_admins`;
-export const MEETING_TEMPLATE = `meeting_template`;
+const ID = `id`;
+const NAME = `name`;
+const DESCRIPTION = `description`;
+const FORWARD_TO_COMMITTEES = `forward_to_committees`;
+const ORGANIZATION_TAGS = `organization_tags`;
+const MANAGERS = `managers`;
+const MEETING_NAME = `meeting_name`;
+const MEETING_START_TIME = `meeting_start_time`;
+const MEETING_END_TIME = `meeting_end_time`;
+const MEETING_ADMINS = `meeting_admins`;
+const MEETING_TEMPLATE = `meeting_template`;
 
 export interface CommitteeCsvPort {
     [ID]: Id;
