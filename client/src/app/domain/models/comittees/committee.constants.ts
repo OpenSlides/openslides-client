40efe59d308d40328cfa59f28926c12d1246d6aa
Elblinator <lelble@intevation.de>
2024-03-26 12:27:03 +0100

    adjust var names and removed unused export

--- client/src/app/domain/models/comittees/committee.constants.ts
+++ client/src/app/domain/models/comittees/committee.constants.ts
@@ -5,38 +5,25 @@ import { Id } from '../../definitions/key-types';
 export const ID = `id`;
 export const NAME = `name`;
 export const DESCRIPTION = `description`;
-export const FORWARD_TO_COMMITTEE_IDS = `forward_to_committees`;
-export const ORGANIZATION_TAG_IDS = `organization_tags`;
-export const MANAGER_IDS = `managers`;
-export const MEETING = `meeting_name`;
-export const MEETING_START_DATE = `meeting_start_time`;
-export const MEETING_END_DATE = `meeting_end_time`;
-export const MEETING_ADMIN_IDS = `meeting_admins`;
-export const MEETING_TEMPLATE_ID = `meeting_template`;
+export const FORWARD_TO_COMMITTEES = `forward_to_committees`;
+export const ORGANIZATION_TAGS = `organization_tags`;
+export const MANAGERS = `managers`;
+export const MEETING_NAME = `meeting_name`;
+export const MEETING_START_TIME = `meeting_start_time`;
+export const MEETING_END_TIME = `meeting_end_time`;
+export const MEETING_ADMINS = `meeting_admins`;
+export const MEETING_TEMPLATE = `meeting_template`;
 
 export interface CommitteeCsvPort {
     [ID]: Id;
     [NAME]: string;
     [DESCRIPTION]?: string;
-    [FORWARD_TO_COMMITTEE_IDS]?: string;
-    [ORGANIZATION_TAG_IDS]?: string;
-    [MANAGER_IDS]?: string | number[];
-    [MEETING]?: string | number;
-    [MEETING_START_DATE]?: string | number;
-    [MEETING_END_DATE]?: string | number;
-    [MEETING_ADMIN_IDS]?: string | number[];
-    [MEETING_TEMPLATE_ID]?: string | number;
+    [FORWARD_TO_COMMITTEES]?: string;
+    [ORGANIZATION_TAGS]?: string;
+    [MANAGERS]?: string | number[];
+    [MEETING_NAME]?: string | number;
+    [MEETING_START_TIME]?: string | number;
+    [MEETING_END_TIME]?: string | number;
+    [MEETING_ADMINS]?: string | number[];
+    [MEETING_TEMPLATE]?: string | number;
 }
-
-export const COMMITTEE_PORT_HEADERS_AND_VERBOSE_NAMES: Partial<CommitteeCsvPort> = {
-    [NAME]: _(`Title`),
-    [DESCRIPTION]: _(`Description`),
-    [FORWARD_TO_COMMITTEE_IDS]: _(`Can forward motions to committee`),
-    [ORGANIZATION_TAG_IDS]: _(`Tags`),
-    [MANAGER_IDS]: _(`Administrators`),
-    [MEETING]: _(`Meeting`),
-    [MEETING_START_DATE]: _(`Start date`),
-    [MEETING_END_DATE]: _(`End date`),
-    [MEETING_ADMIN_IDS]: _(`Meeting administrator`),
-    [MEETING_TEMPLATE_ID]: _(`Meeting template`)
-};
