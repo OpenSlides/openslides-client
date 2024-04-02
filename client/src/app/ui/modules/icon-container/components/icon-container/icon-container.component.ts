fbd72e36c832a986ee6f4675cd3b1b3e3a1e98fa
Elblinator <lelble@intevation.de>
2024-04-02 09:00:18 +0200

    clean up

--- client/src/app/ui/modules/icon-container/components/icon-container/icon-container.component.ts
+++ client/src/app/ui/modules/icon-container/components/icon-container/icon-container.component.ts
@@ -48,12 +48,12 @@ export class IconContainerComponent {
      */
     @Input()
     public swap = false;
-    
+
     /**
-    * Hide span completely
-    */
-   @Input()
-   public hide = false;
+     * Hide span completely
+     */
+    @Input()
+    public hide = false;
 
     /**
      * Boolean to decide, when to show the icon.
