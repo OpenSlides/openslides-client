6f4028e675dfab7fe5b986849a04fe2db15c2973
Elblinator <69210919+Elblinator@users.noreply.github.com>
2024-04-05 10:13:30 +0200

    Update mdc cards (#3487)

--- client/src/app/site/pages/meetings/pages/agenda/modules/topics/pages/topic-detail/topic-detail.module.ts
+++ client/src/app/site/pages/meetings/pages/agenda/modules/topics/pages/topic-detail/topic-detail.module.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
 import { MatLegacyListModule as MatListModule } from '@angular/material/legacy-list';
 import { MatLegacyMenuModule as MatMenuModule } from '@angular/material/legacy-menu';
 import { MatSelectModule } from '@angular/material/select';
--- client/src/app/ui/modules/file-upload/file-upload.module.ts
+++ client/src/app/ui/modules/file-upload/file-upload.module.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
-import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { NgxFileDropModule } from 'ngx-file-drop';
