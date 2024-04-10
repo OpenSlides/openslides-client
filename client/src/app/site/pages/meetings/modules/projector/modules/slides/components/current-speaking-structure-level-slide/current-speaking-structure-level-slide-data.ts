c32dc87586e55b141a6ef7ac83e70a317857a404
Bastian Rihm <brihm@intevation.de>
2024-04-10 13:25:31 +0200

    Extend current speaking structure level slide

--- client/src/app/site/pages/meetings/modules/projector/modules/slides/components/current-speaking-structure-level-slide/current-speaking-structure-level-slide-data.ts
+++ client/src/app/site/pages/meetings/modules/projector/modules/slides/components/current-speaking-structure-level-slide/current-speaking-structure-level-slide-data.ts
@@ -1,7 +1,11 @@
+import { SpeechState } from 'src/app/domain/models/speakers/speech-state';
+
 export interface CurrentSpeakingStructureLevelSlideData {
     id: number;
     name: string;
     color: string;
-    remaining_time: number;
+    remaining_time?: number;
     current_start_time: number;
+    speech_state: SpeechState;
+    point_of_order?: boolean;
 }
