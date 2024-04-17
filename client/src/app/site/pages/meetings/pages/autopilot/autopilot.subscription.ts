e719323ed6a21cf6f60d5ff6cf2cbfb1ce802af6
Bastian Rihm <brihm@intevation.de>
2024-04-17 15:41:24 +0200

    Linter fixes

--- client/src/app/site/pages/meetings/pages/autopilot/autopilot.subscription.ts
+++ client/src/app/site/pages/meetings/pages/autopilot/autopilot.subscription.ts
@@ -1,6 +1,6 @@
 import { Id } from 'src/app/domain/definitions/key-types';
 import { FULL_FIELDSET, MEETING_ROUTING_FIELDS, mergeSubscriptionFollow } from 'src/app/domain/fieldsets/misc';
-import { MeetingUserFieldsets, UserFieldsets } from 'src/app/domain/fieldsets/user';
+import { MeetingUserFieldsets } from 'src/app/domain/fieldsets/user';
 import { SubscriptionConfig, SubscriptionConfigGenerator } from 'src/app/domain/interfaces/subscription-config';
 import { ViewMeeting } from 'src/app/site/pages/meetings/view-models/view-meeting';
 
