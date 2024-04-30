7d2005810eda98824bb368bba38726c3d5f672d2
Bastian Rihm <brihm@intevation.de>
2024-04-30 10:13:37 +0200

    Improve meeting user relation handling (#3573)

--- client/src/app/site/pages/meetings/pages/autopilot/autopilot.subscription.ts
+++ client/src/app/site/pages/meetings/pages/autopilot/autopilot.subscription.ts
@@ -1,6 +1,6 @@
 import { Id } from 'src/app/domain/definitions/key-types';
 import { FULL_FIELDSET, MEETING_ROUTING_FIELDS, mergeSubscriptionFollow } from 'src/app/domain/fieldsets/misc';
-import { MeetingUserFieldsets, UserFieldsets } from 'src/app/domain/fieldsets/user';
+import { MeetingUserFieldsets } from 'src/app/domain/fieldsets/user';
 import { SubscriptionConfig, SubscriptionConfigGenerator } from 'src/app/domain/interfaces/subscription-config';
 import { ViewMeeting } from 'src/app/site/pages/meetings/view-models/view-meeting';
 
@@ -61,13 +61,6 @@ export const getAutopilotContentSubscriptionConfig = (id: Id): SubscriptionConfi
                                         {
                                             idField: `meeting_user_id`,
                                             follow: [
-                                                mergeSubscriptionFollow(
-                                                    { idField: `user_id`, ...UserFieldsets.FullNameSubscription },
-                                                    {
-                                                        idField: `user_id`,
-                                                        fieldset: [`meeting_user_ids`]
-                                                    }
-                                                ),
                                                 {
                                                     idField: `structure_level_ids`,
                                                     fieldset: [`name`, `color`]
