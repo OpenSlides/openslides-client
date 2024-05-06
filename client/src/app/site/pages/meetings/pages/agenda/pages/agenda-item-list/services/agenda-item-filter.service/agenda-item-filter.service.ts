e3499907f8f56f6e6ea2b0ff8ac20157457b21c0
Danny <dreichelt@intevation.de>
2024-05-06 07:26:30 +0200

    cr translation tags not specified

--- client/src/app/site/pages/meetings/pages/agenda/pages/agenda-item-list/services/agenda-item-filter.service/agenda-item-filter.service.ts
+++ client/src/app/site/pages/meetings/pages/agenda/pages/agenda-item-list/services/agenda-item-filter.service/agenda-item-filter.service.ts
@@ -44,7 +44,7 @@ export class AgendaItemFilterService extends BaseMeetingFilterListService<ViewAg
         this.updateFilterForRepo({
             repo: tagRepo,
             filter: this.tagFilterOptions,
-            noneOptionLabel: this.translate.instant(`not specified`)
+            noneOptionLabel: `not specified`
         });
     }
 
