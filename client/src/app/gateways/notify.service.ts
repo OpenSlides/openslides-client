218bc86de351c04fefa3485c06cda66202201839
Bastian Rihm <brihm@intevation.de>
2024-05-21 14:31:44 +0200

    Cleanup

--- client/src/app/gateways/notify.service.ts
+++ client/src/app/gateways/notify.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
-import { OperatorService } from 'src/app/site/services/operator.service';
 
 import { BaseICCGatewayService } from './base-icc-gateway.service';
 
