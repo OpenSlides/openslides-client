bc01c57f3e6fe0c0e6550f609c8c877d70ccec59
Ludwig Reiter <ludwig.reiter@intevation.de>
2024-05-30 14:16:14 +0200

    Add missing type

--- client/src/app/site/pages/meetings/modules/projector/modules/slides/components/current-speaker-chyron-slide/components/current-speaker-chyron-slide.component.ts
+++ client/src/app/site/pages/meetings/modules/projector/modules/slides/components/current-speaker-chyron-slide/components/current-speaker-chyron-slide.component.ts
@@ -9,7 +9,7 @@ import { CurrentSpeakerChyronSlideData } from '../current-speaker-chyron-slide-d
     styleUrls: [`./current-speaker-chyron-slide.component.scss`]
 })
 export class CurrentSpeakerChyronSlideComponent extends BaseSlideComponent<CurrentSpeakerChyronSlideData> {
-    public getSpeaker() {
+    public getSpeaker(): string[] {
         const parts: string[] = [this.data.data.current_speaker_name];
         if (!!this.data.data.current_speaker_level) {
             parts.push(this.data.data.current_speaker_level);
