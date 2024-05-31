9266fa577977255b8ca9cd09444e4c77facc8a60
Ludwig Reiter <ludwig.reiter@intevation.de>
2024-05-31 10:23:28 +0200

    Reorder the user imports fields

--- client/src/app/site/pages/organization/pages/accounts/pages/account-import/definitions/index.ts
+++ client/src/app/site/pages/organization/pages/accounts/pages/account-import/definitions/index.ts
@@ -1,8 +1,18 @@
 import { marker as _ } from '@colsen1991/ngx-translate-extract-marker';
 import { User } from 'src/app/domain/models/users/user';
-import { userHeadersAndVerboseNames } from 'src/app/domain/models/users/user.constants';
 
-export const accountHeadersAndVerboseNames: { [key in keyof User]?: string } = {
-    ...userHeadersAndVerboseNames,
-    default_vote_weight: _(`Vote weight`)
+export const accountHeadersAndVerboseNames: { [key in keyof User]?: any } = {
+    title: _(`Title`),
+    first_name: _(`Given name`),
+    last_name: _(`Surname`),
+    email: _(`Email`),
+    member_number: _(`Member number`),
+    default_vote_weight: _(`Vote weight`),
+    pronoun: _(`Pronoun`),
+    gender: _(`Gender`),
+    username: _(`Username`),
+    default_password: _(`Initial password`),
+    is_active: _(`Active`),
+    is_physical_person: _(`Natural person`),
+    saml_id: _(`SSO identification`)
 };
