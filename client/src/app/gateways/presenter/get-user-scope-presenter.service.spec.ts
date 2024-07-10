5d7010e56e87a2da01e9790ca640e3803ee1535c
Luisa <luisa.beerboom@intevation.de>
2024-07-10 14:06:11 +0200

    cleanup

--- client/src/app/gateways/presenter/get-user-scope-presenter.service.spec.ts
+++ client/src/app/gateways/presenter/get-user-scope-presenter.service.spec.ts
@@ -20,10 +20,6 @@ describe(`GetUserScopePresenterService`, () => {
         8: { title: `superadmin` }
     };
 
-    // function getExpectedCompareResult(key1: keyof typeof scopeMap, key2: keyof typeof scopeMap): number {
-    //     return key1 > key2 ? 1 : -1;
-    // }
-
     const testScopes: { [key in keyof typeof scopeMap]: any } = {
         2: {
             collection: UserScope.MEETING,
@@ -81,40 +77,4 @@ describe(`GetUserScopePresenterService`, () => {
     it(`should correctly call get_user_scope`, async () => {
         expect(await service.call({ user_ids: [2, 3, 5, 6, 7, 8] })).toEqual(testScopes);
     });
-
-    // for (const key of Object.keys(scopeMap).map(x => +x as keyof typeof scopeMap)) {
-    //     const otherKeys: (keyof typeof scopeMap)[] = Object.keys(scopeMap)
-    //         .map(key => +key as keyof typeof scopeMap)
-    //         .filter(otherKey => otherKey !== key);
-    //     it(`test compareScope with oml comparison for ${scopeMap[key].title}`, async () => {
-    //         expect(await service.compareScope(testScopes[key], testScopes[otherKeys[0]], true)).toBe(
-    //             getExpectedCompareResult(key, otherKeys[0])
-    //         );
-    //         expect(await service.compareScope(testScopes[key], testScopes[otherKeys[1]], true)).toBe(
-    //             getExpectedCompareResult(key, otherKeys[1])
-    //         );
-    //         expect(await service.compareScope(testScopes[key], testScopes[otherKeys[2]], true)).toBe(
-    //             getExpectedCompareResult(key, otherKeys[2])
-    //         );
-    //         expect(await service.compareScope(testScopes[key], testScopes[otherKeys[3]], true)).toBe(
-    //             getExpectedCompareResult(key, otherKeys[3])
-    //         );
-    //         expect(await service.compareScope(testScopes[key], testScopes[otherKeys[4]], true)).toBe(
-    //             getExpectedCompareResult(key, otherKeys[4])
-    //         );
-    //         expect(await service.compareScope(testScopes[key], testScopes[key], true)).toBe(0);
-    //         expect(await service.compareScope(testScopes[key], { ...testScopes[key], id: 9 }, true)).toBe(0);
-    //     });
-    // }
-
-    // it(`test compareScope with default settings`, async () => {
-    //     expect(await service.compareScope(testScopes[2], testScopes[3])).toBe(-1);
-    //     expect(await service.compareScope(testScopes[2], testScopes[5])).toBe(-1);
-    //     expect(await service.compareScope(testScopes[3], testScopes[5])).toBe(-1);
-    //     expect(await service.compareScope(testScopes[3], testScopes[2])).toBe(1);
-    //     expect(await service.compareScope(testScopes[5], testScopes[2])).toBe(1);
-    //     expect(await service.compareScope(testScopes[5], testScopes[3])).toBe(1);
-    //     expect(await service.compareScope(testScopes[7], testScopes[6])).toBe(0);
-    //     expect(await service.compareScope(testScopes[6], testScopes[6])).toBe(0);
-    // });
 });
