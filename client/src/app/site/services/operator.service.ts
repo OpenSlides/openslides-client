9765e327a475d6b2136d806f1a7c839dcf90ae47
Bastian Rihm <brihm@intevation.de>
2024-07-31 08:49:58 +0200

    Enable permission checks for superadmin in locked meetings

--- client/src/app/site/services/operator.service.ts
+++ client/src/app/site/services/operator.service.ts
@@ -537,7 +537,7 @@ export class OperatorService {
             // console.warn(`has perms: Usage outside of meeting!`);
             return false;
         }
-        if (this.isSuperAdmin) {
+        if (this.isSuperAdmin && !this.activeMeeting.locked_from_inside) {
             return true;
         }
 
@@ -561,7 +561,7 @@ export class OperatorService {
             // console.warn(`has perms: Operator is not ready!`);
             return false;
         }
-        if (this.isSuperAdmin) {
+        if (this.isSuperAdmin && !this.activeMeeting.locked_from_inside) {
             return true;
         }
         const groups = this.user.groups(meetingId);
