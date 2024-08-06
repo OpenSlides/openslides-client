5b5182337b6609b7309c048fe1b58d9bf9a774c9
Bastian Rihm <brihm@intevation.de>
2024-08-06 14:38:23 +0200

    Add chart component

---
+++ client/src/app/site/pages/meetings/modules/poll/components/poll-filtered-votes-chart/poll-filtered-votes-chart.component.ts
@@ -0,0 +1,53 @@
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { VoteValue } from 'src/app/domain/models/poll';
+import { BaseUiComponent } from 'src/app/ui/base/base-ui-component';
+
+import { ViewVote } from '../../../../pages/polls';
+import { VotesFilterService } from '../../services/votes-filter.service';
+
+interface VoteAmount {
+    value: VoteValue;
+    amount: number;
+    weightedAmount: number;
+}
+
+@Component({
+    selector: `os-poll-filted-votes-chart`,
+    templateUrl: `./poll-filtered-votes-chart.component.html`,
+    styleUrls: [`./poll-filtered-votes-chart.component.scss`],
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PollFilteredVotesChartComponent extends BaseUiComponent implements OnInit {
+    public totalAmount = 0;
+    public totalAmountWeighted = 0;
+    public voteAmounts: VoteAmount[] = [];
+
+    public constructor(
+        private filterService: VotesFilterService,
+        private cd: ChangeDetectorRef
+    ) {
+        super();
+    }
+
+    public ngOnInit(): void {
+        this.subscriptions.push(this.filterService.outputObservable.subscribe(votes => this.onVotesUpdated(votes)));
+    }
+
+    private onVotesUpdated(votes: ViewVote[]): void {
+        this.voteAmounts = [];
+        const voteValues: VoteValue[] = [`Y`, `N`, `A`];
+        for (const i in voteValues) {
+            const voteValue = voteValues[i];
+            this.voteAmounts.push({
+                value: voteValue,
+                amount: votes.reduce((acc, curr) => acc + +(curr.value === voteValue), 0),
+                weightedAmount: votes.reduce((acc, curr) => acc + (curr.value === voteValue ? +curr.weight : 0), 0)
+            });
+        }
+
+        this.totalAmount = votes.length;
+        this.totalAmountWeighted = votes.reduce((acc, curr) => acc + +curr.weight, 0);
+
+        this.cd.markForCheck();
+    }
+}
