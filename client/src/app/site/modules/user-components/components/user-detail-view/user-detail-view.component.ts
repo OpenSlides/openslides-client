76d54b2826a5688be226484d8863bc3f2d2da317
Bastian Rihm <brihm@intevation.de>
2024-08-28 16:05:25 +0200

    Fix wrong value in user form if `is_active` empty

--- client/src/app/site/modules/user-components/components/user-detail-view/user-detail-view.component.ts
+++ client/src/app/site/modules/user-components/components/user-detail-view/user-detail-view.component.ts
@@ -239,13 +239,11 @@ export class UserDetailViewComponent extends BaseUiComponent implements OnInit, 
         Object.keys(this.personalInfoForm.controls).forEach(ctrl => {
             personalInfoPatch[ctrl] = this.getFormValuePatch(ctrl as keyof ViewUser);
         });
-        const isActiveExists = typeof this.user?.is_active === `boolean`;
-        const isPersonExists = typeof this.user?.is_physical_person === `boolean`;
         this.personalInfoForm.patchValue(
             {
                 ...personalInfoPatch,
-                ...(isActiveExists ? {} : { is_active: true }),
-                ...(isPersonExists ? {} : { is_physical_person: true })
+                ...(this.user?.id ? {} : { is_active: true }),
+                ...(this.user?.id ? {} : { is_physical_person: true })
             },
             { emitEvent: false }
         );
