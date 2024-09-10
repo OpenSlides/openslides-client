45646dc7af5990933559302454156fa18490d56e
Elblinator <69210919+Elblinator@users.noreply.github.com>
2024-09-10 16:38:40 +0200

    Public organization mediafiles (#4049)

--- client/src/app/site/pages/organization/pages/mediafiles/modules/organization-mediafile-list/components/organization-mediafile-list/organization-mediafile-list.component.ts
+++ client/src/app/site/pages/organization/pages/mediafiles/modules/organization-mediafile-list/components/organization-mediafile-list/organization-mediafile-list.component.ts
@@ -69,6 +69,10 @@ export class OrganizationMediafileListComponent
         return this.canEdit;
     }
 
+    public get isOrgaLevelAndRootLevel(): boolean {
+        return this.directoryChain.length == 0;
+    }
+
     /**
      * The form to edit Files
      */
