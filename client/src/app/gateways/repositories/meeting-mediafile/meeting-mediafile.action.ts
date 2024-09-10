111820e646303f3775ab89172c6ce19deef448fb
Elblinator <lelble@intevation.de>
2024-09-10 15:14:24 +0200

    MeetingMediafileAction

--- client/src/app/gateways/repositories/meeting-mediafile/meeting-mediafile.action.ts
+++
@@ -1,6 +0,0 @@
-export class MeetingMediafileAction {
-    public static readonly CREATE_FILE = `mediafile.upload`;
-    public static readonly CREATE_DIRECTORY = `mediafile.create_directory`;
-    public static readonly UPDATE = `mediafile.update`;
-    public static readonly DELETE = `mediafile.delete`;
-}
