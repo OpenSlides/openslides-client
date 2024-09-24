bbb853f73d2a87dbbb9c575827ac21eacd24a75a
Elblinator <lelble@intevation.de>
2024-09-24 10:07:41 +0200

    Add poll method

--- client/src/app/site/pages/meetings/services/meeting-settings-definition.service/meeting-settings-definitions.ts
+++ client/src/app/site/pages/meetings/services/meeting-settings-definition.service/meeting-settings-definitions.ts
@@ -13,6 +13,7 @@ import { createEmailValidator } from 'src/app/infrastructure/utils/validators/em
 
 import { OrganizationSettingsService } from '../../../organization/services/organization-settings.service';
 import { AssignmentPollMethodVerbose } from '../../pages/assignments/modules/assignment-poll/definitions';
+import { MotionPollMethodVerbose } from '../../pages/motions/modules/motion-poll';
 import { ViewMeeting } from '../../view-models/view-meeting';
 
 export type SettingsValueMap = { [key in keyof Settings]?: any };
@@ -765,6 +766,12 @@ export const meetingSettings: SettingsGroup[] = fillInSettingsDefaults([
                         key: `motion_poll_default_group_ids`,
                         label: _(`Default groups with voting rights`),
                         type: `groups`
+                    },
+                    {
+                        key: `motion_poll_default_method`,
+                        label: _(`Default vote method`),
+                        type: `choice`,
+                        choices: MotionPollMethodVerbose
                     },
                     {
                         key: `motion_poll_default_onehundred_percent_base`,
