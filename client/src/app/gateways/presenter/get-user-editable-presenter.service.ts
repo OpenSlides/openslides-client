ef12f8d0573d6c122b6ef36697eb06800a6c8cee
Ludwig Reiter <ludwig.reiter@intevation.de>
2024-10-01 13:25:59 +0200

    Add get-user-editable presenter

---
+++ client/src/app/gateways/presenter/get-user-editable-presenter.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Id } from 'src/app/domain/definitions/key-types';
+
+import { Presenter } from './presenter';
+import { PresenterService } from './presenter.service';
+
+interface GetUserEditablePresenterPayload {
+    user_ids: Id[];
+    fields: string[];
+}
+
+interface GetUserEditableIdentifiedScope {
+    editable: boolean;
+    message?: string;
+}
+
+export interface GetUserEditablePresenterResult {
+    [user_id: Id]: GetUserEditableIdentifiedScope;
+}
+
+@Injectable({
+    providedIn: `root`
+})
+export class GetUserEditablePresenterService {
+    public constructor(private presenter: PresenterService) {}
+
+    public async call(payload: GetUserEditablePresenterPayload): Promise<GetUserEditablePresenterResult> {
+        return await this.presenter.call(Presenter.GET_USER_EDITABLE, payload);
+    }
+}
