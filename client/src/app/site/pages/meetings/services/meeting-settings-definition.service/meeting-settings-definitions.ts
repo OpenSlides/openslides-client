ca06eaa944e4030b4e02912a1e623500977f6886
anbebe <a.bebenroth@icloud.com>
2024-11-15 16:44:14 +0100

    Change wording of warn text

--- client/src/app/site/pages/meetings/services/meeting-settings-definition.service/meeting-settings-definitions.ts
+++ client/src/app/site/pages/meetings/services/meeting-settings-definition.service/meeting-settings-definitions.ts
@@ -216,7 +216,9 @@ export const meetingSettings: SettingsGroup[] = fillInSettingsDefaults([
                         helpText: _(
                             `Enables public access to this meeting without login data. Permissions can be set after activation in the new group 'Public'.`
                         ),
-                        warnText: _(`The public access setting is deactivated for this meeting.`),
+                        warnText: _(
+                            `The public access setting is deactivated for the organization. Please contact your admins or hosting providers to activate the setting.`
+                        ),
                         warn: orgaSettings => !orgaSettings.instant(`enable_anonymous`)
                     }
                 ]
