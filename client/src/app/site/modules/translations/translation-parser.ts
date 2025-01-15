5cae79a78b375ee5351cac6f330ebb0f548d43f4
Bastian Rihm <brihm@intevation.de>
2025-01-15 11:22:52 +0100

    Load translations into parser

--- client/src/app/site/modules/translations/translation-parser.ts
+++ client/src/app/site/modules/translations/translation-parser.ts
@@ -1,4 +1,5 @@
 import { InterpolateFunction, InterpolationParameters, TranslateDefaultParser } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 
 import { CustomTranslationService } from './custom-translation.service';
 
@@ -6,19 +7,25 @@ import { CustomTranslationService } from './custom-translation.service';
  * Translation parser that adds custom translations
  */
 export class CustomTranslationParser extends TranslateDefaultParser {
+    private static ctSubscription: Subscription;
+    private static ct: { [key: string]: string } = {};
+
     public constructor(private ctService: CustomTranslationService) {
         super();
+
+        if (!CustomTranslationParser.ctSubscription) {
+            CustomTranslationParser.ctSubscription = this.ctService.customTranslationSubject.subscribe(ct => {
+                CustomTranslationParser.ct = ct || {};
+            });
+        }
     }
 
     public override interpolate(
         expr: InterpolateFunction | string,
         params?: InterpolationParameters
     ): string | undefined {
-        if (typeof expr === `string` && this.ctService.customTranslationSubject.value) {
-            const ct = this.ctService.customTranslationSubject.value[expr];
-            if (ct) {
-                return ct;
-            }
+        if (typeof expr === `string` && CustomTranslationParser.ct[expr]) {
+            return CustomTranslationParser.ct[expr];
         }
 
         return super.interpolate(expr, params);
