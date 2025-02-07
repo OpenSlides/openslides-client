6db383be36af1599f49cd8e6666ee9961119fdd6
Ludwig Reiter <ludwig.reiter@intevation.de>
2025-02-07 13:49:55 +0100

    Make openslides-overlay standalone

--- client/src/app/site/pages/meetings/pages/interaction/services/interaction-service.module.ts
+++ client/src/app/site/pages/meetings/pages/interaction/services/interaction-service.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
-import { OpenSlidesOverlayModule } from 'src/app/ui/modules/openslides-overlay';
+import { OverlayComponent } from 'src/app/ui/modules/openslides-overlay';
 import { PromptDialogModule } from 'src/app/ui/modules/prompt-dialog';
 
 @NgModule({
-    imports: [PromptDialogModule, OpenSlidesOverlayModule]
+    imports: [PromptDialogModule, OverlayComponent]
 })
 export class InteractionServiceModule {}
