441b0fae684015a193b5b2dce8d149dd3d52c80e
anbebe <58171814+anbebe@users.noreply.github.com>
2025-03-10 09:35:45 +0100

    Fix approximation for pdf scrolling (#4719)

--- client/src/app/site/pages/meetings/pages/projectors/modules/projector-detail/components/projector-detail/projector-detail.component.ts
+++ client/src/app/site/pages/meetings/pages/projectors/modules/projector-detail/components/projector-detail/projector-detail.component.ts
@@ -109,6 +109,10 @@ export class ProjectorDetailComponent extends BaseMeetingComponent implements On
         return Math.floor(this.projector.height / 100);
     }
 
+    /** stores the decimal places that could not be scrolled
+     *  at once for the projector height */
+    private summedOverflowSteps: number;
+
     private _hasEnoughWiFiData: boolean;
 
     private _noWiFiData: boolean;
@@ -160,6 +164,7 @@ export class ProjectorDetailComponent extends BaseMeetingComponent implements On
         this.projectorObservable = this._projectorIdSubject.pipe(
             switchMap(projectorId => this.repo.getViewModelObservable(projectorId))
         );
+        this.summedOverflowSteps = 0;
     }
 
     public onIdFound(id: Id | null): void {
@@ -367,5 +372,22 @@ export class ProjectorDetailComponent extends BaseMeetingComponent implements On
                 }
             })
         );
+    }
+
+    public scrollPDFPage(direction: ScrollScaleDirection): void {
+        if (this.projector.scroll <= 0) {
+            this.summedOverflowSteps = 0;
+        }
+        this.summedOverflowSteps +=
+            ((this.projector.height % 100) / 100) * (direction === ScrollScaleDirection.Up ? 1 : -1);
+        if (this.summedOverflowSteps >= 0.5) {
+            this.scroll(direction, this.projectorHeight + 1);
+            this.summedOverflowSteps -= 1;
+        } else if (this.summedOverflowSteps <= -0.5) {
+            this.scroll(direction, this.projectorHeight + 1);
+            this.summedOverflowSteps += 1;
+        } else {
+            this.scroll(direction, this.projectorHeight);
+        }
     }
 }
