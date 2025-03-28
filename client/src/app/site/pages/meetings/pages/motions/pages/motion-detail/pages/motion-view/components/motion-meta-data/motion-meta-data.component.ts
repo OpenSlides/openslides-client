54fd6bd9c59f62d9b936261dbf17e55aefe5bac5
anbebe <58171814+anbebe@users.noreply.github.com>
2025-03-28 12:54:42 +0100

    Disable forwarding of single amendments (#4818)

--- client/src/app/site/pages/meetings/pages/motions/pages/motion-detail/pages/motion-view/components/motion-meta-data/motion-meta-data.component.ts
+++ client/src/app/site/pages/meetings/pages/motions/pages/motion-detail/pages/motion-view/components/motion-meta-data/motion-meta-data.component.ts
@@ -185,7 +185,7 @@ export class MotionMetaDataComponent extends BaseMotionDetailChildComponent impl
 
         if (operator.hasPerms(Permission.motionCanManageMetadata)) {
             this.motionForwardingService.forwardingMeetingsAvailable().then(forwardingAvailable => {
-                this._forwardingAvailable = forwardingAvailable;
+                this._forwardingAvailable = forwardingAvailable && !this.motion.isAmendment();
                 this.cd.markForCheck();
                 this.loadForwardingCommittees = async (): Promise<Selectable[]> => {
                     return (await this.checkPresenter()) as Selectable[];
