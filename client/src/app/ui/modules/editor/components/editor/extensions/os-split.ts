865c7322f72470d74e4b166b315eb915465e75e8
Bastian Rihm <brihm@intevation.de>
2025-04-23 12:39:30 +0200

    Prevent list modification if os-split-* is set

--- client/src/app/ui/modules/editor/components/editor/extensions/os-split.ts
+++ client/src/app/ui/modules/editor/components/editor/extensions/os-split.ts
@@ -1,4 +1,4 @@
-import { Extension } from "@tiptap/core";
+import { CommandProps, Extension } from "@tiptap/core";
 import BulletList from '@tiptap/extension-bullet-list';
 import ListItem from '@tiptap/extension-list-item';
 import OrderedList from '@tiptap/extension-ordered-list';
@@ -35,7 +35,7 @@ export const OsSplit = Extension.create({
                     const head = tr.selection.$head;
                     const classNames: string = head.node(head.depth - 3).attrs[`class`] ?? ``;
                     if (classNames.indexOf(`os-split-`) !== -1) {
-                        return true;
+                        return false;
                     }
                     return commands.liftListItem(name);
                 }
@@ -60,11 +60,45 @@ export const OsSplitListItem = ListItem.extend({
 export const OsSplitBulletList = BulletList.extend({
     content() {
         return `${this.options.itemTypeName}+ paragraph*`;
+    },
+
+    addCommands() {
+        return {
+            toggleBulletList: () => (args: CommandProps): boolean => {
+                const { tr } = args;
+                const head = tr.selection.$head;
+                if (head) {
+                    const classNames: string = head.node(head.depth - 3)?.attrs[`class`] ?? ``;
+                    if (classNames.indexOf(`os-split-`) !== -1) {
+                        return false;
+                    }
+                }
+
+                return this.parent().toggleBulletList()(args);
+            }
+        };
     }
 });
 
 export const OsSplitOrderedList = OrderedList.extend({
     content() {
         return `${this.options.itemTypeName}+ paragraph*`;
+    },
+
+    addCommands() {
+        return {
+            toggleOrderedList: () => (args: CommandProps): boolean => {
+                const { tr } = args;
+                const head = tr.selection.$head;
+                if (head) {
+                    const classNames: string = head.node(head.depth - 3)?.attrs[`class`] ?? ``;
+                    if (classNames.indexOf(`os-split-`) !== -1) {
+                        return false;
+                    }
+                }
+
+                return this.parent().toggleOrderedList()(args);
+            }
+        };
     }
 });
