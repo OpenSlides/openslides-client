92cedc69ff44df6feb11ad5ab8f4b693ad20a3ea
Bastian Rihm <brihm@intevation.de>
2025-04-23 11:26:29 +0200

    Fix test

--- client/src/app/site/pages/meetings/modules/poll/pipes/poll-parse-number/poll-parse-number.pipe.ts
+++ client/src/app/site/pages/meetings/modules/poll/pipes/poll-parse-number/poll-parse-number.pipe.ts
@@ -1,14 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-import { MotionPollService } from '../../../../pages/motions/modules/motion-poll/services';
+import { PollService } from '../../services/poll.service';
 
 @Pipe({
     name: `pollParseNumber`
 })
 export class PollParseNumberPipe implements PipeTransform {
-    public constructor(private motionPollService: MotionPollService) {}
+    public constructor(private pollService: PollService) {}
 
     public transform(value?: number): string {
-        return this.motionPollService.parseNumber(value);
+        return this.pollService.parseNumber(value);
     }
 }
