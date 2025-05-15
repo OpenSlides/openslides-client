172e95ad1f559f244bfeccd7eec14fb8cae3ab08
Emanuel Schütze <emanuel@intevation.de>
2025-05-15 10:50:46 +0200

    Updated translations (#4982)

--- client/src/app/site/pages/meetings/pages/motions/modules/motion-poll/components/motion-poll-form/motion-poll-form.component.ts
+++ client/src/app/site/pages/meetings/pages/motions/modules/motion-poll/components/motion-poll-form/motion-poll-form.component.ts
@@ -15,7 +15,7 @@ import {
     standalone: false
 })
 export class MotionPollFormComponent extends BasePollFormComponent {
-    public override PollPropertyVerbose: Record<PollPropertyVerboseKey, string> = { ...this.PollPropertyVerbose, pollmethod: _(`Poll method`) };
+    public override PollPropertyVerbose: Record<PollPropertyVerboseKey, string> = { ...this.PollPropertyVerbose, pollmethod: _(`Voting method`) };
     public get hideSelects(): PollFormHideSelectsData {
         return {
             globalOptions: true
