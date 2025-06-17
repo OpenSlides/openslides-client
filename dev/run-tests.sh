3580538c0cafa3a79662d1539c948d5e1fcf6d58
Jan Malte Behrens <janmtbehrens@gmail.com>
2025-06-17 13:48:14 +0200

    Small fixes, Typos and Standards

--- dev/run-tests.sh
+++ dev/run-tests.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-# Executes all tests. Should errors occur, CATCH will be set to 1, causing an erronous exit code.
+# Executes all tests. Should errors occur, CATCH will be set to 1, causing an erroneous exit code.
 
 echo "########################################################################"
 echo "###################### Run Tests and Linters ###########################"
 echo "########################################################################"
 
-IMAGE_TAG=openslides-client-tests
-CATCH=0
+# Parameters
 PERSIST_CONTAINERS=$1
 
-# Linters
+# Setup
+IMAGE_TAG=openslides-client-tests
+CATCH=0
+
+# Execution
 if [ "$(docker images -q $IMAGE_TAG)" = "" ]; then make build-test || CATCH=1; fi
 docker run -t ${IMAGE_TAG} /bin/sh -c "apk add chromium && npm run test-silently -- --browsers=ChromiumHeadlessNoSandbox" || CATCH=1
 docker run -t ${IMAGE_TAG} npm run lint || CATCH=1
