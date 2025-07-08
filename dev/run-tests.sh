3b008b85b359017d08c73b18db357486f24afbda
Jan Malte Behrens <janmtbehrens@gmail.com>
2025-07-08 15:22:15 +0200

    Adding trap to all tests

--- dev/run-tests.sh
+++ dev/run-tests.sh
@@ -6,19 +6,17 @@ echo "########################################################################"
 echo "###################### Run Tests and Linters ###########################"
 echo "########################################################################"
 
-# Parameters
-PERSIST_CONTAINERS=$1
-
 # Setup
 IMAGE_TAG=openslides-client-tests
 CATCH=0
+
+# Safe Exit
+trap 'docker stop $(docker ps -a -q --filter ancestor=${IMAGE_TAG})' EXIT
 
 # Execution
 if [ "$(docker images -q $IMAGE_TAG)" = "" ]; then make build-test || CATCH=1; fi
 docker run -t ${IMAGE_TAG} /bin/sh -c "apk add chromium && npm run test-silently -- --browsers=ChromiumHeadlessNoSandbox" || CATCH=1
 docker run -t ${IMAGE_TAG} npm run lint || CATCH=1
 docker run -t ${IMAGE_TAG} npm run prettify-check || CATCH=1
-
-if [ -z "$PERSIST_CONTAINERS" ]; then docker stop $(docker ps -a -q --filter ancestor=${IMAGE_TAG} --format="{{.ID}}") || CATCH=1; fi
 
 exit $CATCH
