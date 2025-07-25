274726e9b491350275d4116e920800c9568bbcbd
Bastian Rihm <brihm@intevation.de>
2025-07-25 17:09:32 +0200

    Add package test workflow

--- packages/openslides-motion-diff/vite.config.ts
+++ packages/openslides-motion-diff/vite.config.ts
@@ -9,6 +9,12 @@ export default defineConfig({
     typecheck: {
       enabled: true,
     },
-    environment: 'jsdom'
+    environment: 'jsdom',
+    coverage: {
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ['text', 'json-summary', 'json'],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
+    }
   },
 });
