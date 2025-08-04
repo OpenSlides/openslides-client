4f7664ae4c6a63111b616309e080f8fafd9a6642
Ludwig Reiter <ludwig.reiter@intevation.de>
2025-08-04 15:02:51 +0200

    Add dialogForm and first chip items

--- client/src/app/site/pages/meetings/pages/agenda/pages/agenda-export/components/agenda-export/agenda-export.component.ts
+++ client/src/app/site/pages/meetings/pages/agenda/pages/agenda-export/components/agenda-export/agenda-export.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
@@ -12,6 +12,8 @@ import { BaseComponent } from 'src/app/site/base/base.component';
 import { OpenSlidesTranslationModule } from 'src/app/site/modules/translations';
 import { DirectivesModule } from 'src/app/ui/directives';
 import { HeadBarModule } from 'src/app/ui/modules/head-bar';
+
+import { ExportFileFormat } from '../../../../../motions/services/export/definitions';
 
 @Component({
     selector: `os-agenda-export`,
@@ -33,8 +35,39 @@ import { HeadBarModule } from 'src/app/ui/modules/head-bar';
     ]
 })
 export class AgendaExportComponent extends BaseComponent {
+    public dialogForm!: UntypedFormGroup;
+
+    public get isPDFFormat(): boolean {
+        return this.fileFormats[this.tabIndex] === ExportFileFormat.PDF;
+    }
+
+    public get isCSVFormat(): boolean {
+        return this.fileFormats[this.tabIndex] === ExportFileFormat.CSV;
+    }
+
+    private tabIndex = 0;
+    // Store fileformats with corresponding tab group index
+    private fileFormats: ExportFileFormat[] = [ExportFileFormat.PDF, ExportFileFormat.CSV];
+
+    public constructor(private formBuilder: UntypedFormBuilder) {
+        super();
+        this.initForm();
+    }
+
     public cancelExport(): void {}
     public exportAgenda(): void {}
     public afterTabChanged(): void {}
-    public tabChanged(_event: any): void {}
+    public tabChanged(_event: any): void {
+        this.tabIndex = _event.index;
+    }
+
+    private initForm(): void {
+        this.dialogForm = this.formBuilder.group({
+            format: [],
+            content: [],
+            metaInfo: [],
+            pageLayout: [],
+            headerFooter: []
+        });
+    }
 }
