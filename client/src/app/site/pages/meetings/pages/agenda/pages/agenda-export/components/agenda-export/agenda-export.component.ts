36e14fc1fac0279dc20ad06054798274a14ba313
Ludwig Reiter <ludwig.reiter@intevation.de>
2025-08-04 13:24:16 +0200

    Initial commit, page, draft

---
+++ client/src/app/site/pages/meetings/pages/agenda/pages/agenda-export/components/agenda-export/agenda-export.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+    selector: `os-agenda-export`,
+    imports: [],
+    templateUrl: `./agenda-export.component.html`,
+    styleUrl: `./agenda-export.component.scss`,
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AgendaExportComponent {
+
+}
