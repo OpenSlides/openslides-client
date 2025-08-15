ae10f58b24210a05e1b2b991862ccd013d6f96b4
Ludwig Reiter <ludwig.reiter@intevation.de>
2025-08-15 08:16:13 +0200

    Rm last speaker option and surplus code

--- client/src/app/site/pages/meetings/pages/agenda/pages/agenda-item-list/services/agenda-item-export.service/agenda-item-export.service.ts
+++ client/src/app/site/pages/meetings/pages/agenda/pages/agenda-item-list/services/agenda-item-export.service/agenda-item-export.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { Content } from 'pdfmake/interfaces';
-import { OSTreeNode } from 'src/app/infrastructure/definitions/tree';
 import { ViewAgendaItem } from 'src/app/site/pages/meetings/pages/agenda';
 import { MeetingPdfExportService } from 'src/app/site/pages/meetings/services/export';
 import { MeetingCsvExportForBackendService } from 'src/app/site/pages/meetings/services/export/meeting-csv-export-for-backend.service';
@@ -9,14 +7,6 @@ import { TreeService } from 'src/app/ui/modules/sorting/modules/sorting-tree/ser
 
 import { AgendaPdfCatalogExportService } from '../../../../services/agenda-pdf-catalog-export.service/agenda-pdf-catalog-export.service';
 import { AgendaItemListServiceModule } from '../agenda-item-list-service.module';
-
-/**
- * pdfMake structure for a content line in the pdf document.
- */
-interface AgendaTreePdfEntry {
-    style: string;
-    columns: { width?: number; text: string }[];
-}
 
 export enum ExportFileFormat {
     PDF = 0,
@@ -112,72 +102,5 @@ export class AgendaItemExportService {
             docDefinition: this.agendaPdfExportService.agendaListToDocDef(source, info, meta),
             filename
         });
-    }
-
-    /**
-     * Creates pdfMake definitions for a agenda list pdf from the given agenda items
-     *
-     * @param items A list of viewItems to be included in this agenda list. Items with the property 'hidden'
-     * will be ignored, all other items will be sorted by their parents and weight
-     * @returns definitions ready to be opened or exported via {@link PdfDocumentService}
-     */
-    private agendaListToDocDef(items: ViewAgendaItem[]): Content[] {
-        const tree: OSTreeNode<ViewAgendaItem>[] = this.treeService.makeSortedTree(items, `weight`, `parent_id`);
-        const title = {
-            text: this.translate.instant(`Agenda`),
-            style: `title`
-        };
-        const entries = this.createEntries(tree);
-        return [title, entries];
-    }
-
-    /**
-     * Traverses the given nodeTree and creates an array of entries for all items
-     *
-     * @param tree
-     * @returns hierarchical pdfMake definitions for topic entries
-     */
-    private createEntries(tree: OSTreeNode<ViewAgendaItem>[]): AgendaTreePdfEntry[] {
-        const content: AgendaTreePdfEntry[] = [];
-        tree.forEach(treeitem => content.push(...this.parseItem(treeitem, 0)));
-        return content;
-    }
-
-    /**
-     * Parses an entry line and triggers parsing of any children
-     * Items with 'is_hidden' and their subitems are not exported
-     *
-     * @param nodeItem the item for the head line
-     * @param level: The hierarchy index (beginning at 0 for top level agenda topics)
-     * @returns pdfMake definitions for the number/title strings, indented according to hierarchy
-     */
-    private parseItem(nodeItem: OSTreeNode<ViewAgendaItem>, level: number): AgendaTreePdfEntry[] {
-        const itemList: AgendaTreePdfEntry[] = [];
-        if (!nodeItem.item.item.is_hidden) {
-            // don't include hidden items and their subitems
-            const resultString: AgendaTreePdfEntry = {
-                style: level ? `listChild` : `listParent`,
-                columns: [
-                    {
-                        width: level * 15,
-                        text: ``
-                    },
-                    {
-                        width: 60,
-                        text: nodeItem.item.item_number || ``
-                    },
-                    {
-                        text: nodeItem.item.content_object!.getTitle()
-                    }
-                ]
-            };
-            itemList.push(resultString);
-            if (nodeItem.children && nodeItem.children.length) {
-                nodeItem.children.forEach(child => {
-                    itemList.push(...this.parseItem(child, level + 1));
-                });
-            }
-        }
-        return itemList;
     }
 }
