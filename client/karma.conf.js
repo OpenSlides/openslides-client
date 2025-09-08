423883c50a62493903e10bf8a27276aac232a708
Bastian Rihm <brihm@intevation.de>
2025-09-08 15:09:23 +0200

    Fix karma tests

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -10,8 +10,7 @@ module.exports = function(config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      
+      require('karma-coverage')
     ],
     client: {
       jasmine: {
