8e63f8da2d90ad3f92d97214f47ca636c55c0dac
Bastian Rihm <brihm@intevation.de>
2025-10-09 14:12:15 +0200

    Fix error

--- packages/openslides-motion-diff/src/compat/migrations/v1.0.0.ts
+++ packages/openslides-motion-diff/src/compat/migrations/v1.0.0.ts
@@ -4,7 +4,7 @@ import * as HtmlDiff from "../../diff";
 export function applyV1_0_0(ln: typeof LineNumbering, _diff: typeof HtmlDiff) {
   const originalLnInsert = ln.insert;
   ln.insert = function(input): string {
-    input.html = input.html.replace(/<br>(<br>)+/gm, (match) => {
+    input.html = input.html?.replace(/<br>(<br>)+/gm, (match) => {
       return `<br style="margin-bottom: ${match.length / 4 * 19}px">`;
     });
 
