9b6033fc9da2e6514524f089d020569a72a16aaa
Elblinator <lelble@intevation.de>
2026-01-16 14:38:49 +0100

    See if option is available

--- client/src/app/ui/modules/editor/components/editor/editor.component.ts
+++ client/src/app/ui/modules/editor/components/editor/editor.component.ts
@@ -325,21 +325,26 @@ export class EditorComponent extends BaseFormControlComponent<string> implements
     }
 
     public clearSelectedFormat(): void {
-        try {
-            this.editor
-                .chain()
-                .focus()
-                .selectAll()
-                .unsetHighlight()
-                .unsetStrike()
-                .unsetItalic()
-                .unsetUnderline()
-                .unsetColor()
-                .blur()
-                .run();
-        } catch (e) {}
+        if (this.isExtensionActive(`highlight`)) {
+            this.editor.commands.unsetHighlight();
+        }
+        if (this.isExtensionActive(`color`)) {
+            this.editor.commands.unsetColor();
+        }
+        if (this.isExtensionActive(`strike`)) {
+            this.editor.commands.unsetStrike();
+        }
+        if (this.isExtensionActive(`underline`)) {
+            this.editor.commands.unsetUnderline();
+        }
+        if (this.isExtensionActive(`italic`)) {
+            this.editor.commands.unsetItalic();
+        }
+        if (this.isExtensionActive(`bold`)) {
+            this.editor.commands.unsetBold();
+        }
 
-        this.editor.chain().focus().selectAll().unsetBold().removeEmptyTextStyle().blur().run();
+        this.editor.commands.removeEmptyTextStyle();
     }
 
     public async setLinkDialog(): Promise<void> {
