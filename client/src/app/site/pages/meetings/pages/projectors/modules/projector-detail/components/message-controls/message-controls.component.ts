2bce097b57db4eb2dae9db8827ead58b4937d0fc
Elblinator <lelble@intevation.de>
2026-01-30 11:44:34 +0100

    Change message behaviour

--- client/src/app/site/pages/meetings/pages/projectors/modules/projector-detail/components/message-controls/message-controls.component.ts
+++ client/src/app/site/pages/meetings/pages/projectors/modules/projector-detail/components/message-controls/message-controls.component.ts
@@ -1,11 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { ProjectionDialogService } from 'src/app/site/pages/meetings/modules/meetings-component-collector/projection-dialog/services/projection-dialog.service';
 import { ViewProjector, ViewProjectorMessage } from 'src/app/site/pages/meetings/pages/projectors';
 import { PromptService } from 'src/app/ui/modules/prompt-dialog';
 
 import { ProjectorMessageDialogService } from '../../../../components/projector-message-dialog/services/projector-message-dialog.service';
-import { ProjectorControllerService } from '../../../../services/projector-controller.service';
 import { ProjectorMessageControllerService } from '../../services/projector-message-controller.service';
 
 @Component({
@@ -14,7 +12,7 @@ import { ProjectorMessageControllerService } from '../../services/projector-mess
     styleUrls: [`./message-controls.component.scss`],
     standalone: false
 })
-export class MessageControlsComponent implements OnInit {
+export class MessageControlsComponent {
     /**
      * Input slot for the projector message model
      */
@@ -27,21 +25,12 @@ export class MessageControlsComponent implements OnInit {
     @Input()
     public projector!: ViewProjector;
 
-    public isProjected = false;
-
     public constructor(
         private translate: TranslateService,
         private repo: ProjectorMessageControllerService,
         private promptService: PromptService,
-        private projectionDialogService: ProjectionDialogService,
-        private projectorService: ProjectorControllerService,
         private dialog: ProjectorMessageDialogService
     ) {}
-
-    public ngOnInit(): void {
-        const projections = this.projectorService.getMatchingProjectionsFromProjector(this.message, this.projector);
-        this.isProjected = !!projections.length;
-    }
 
     /**
      * Fires an edit event
@@ -58,15 +47,6 @@ export class MessageControlsComponent implements OnInit {
                 this.repo.update(update, this.message);
             }
         });
-    }
-
-    /**
-     * Brings the projection dialog
-     */
-    public onBringDialog(): void {
-        const projections = this.projectorService.getMatchingProjectionsFromProjector(this.message, this.projector);
-        this.isProjected = !!projections.length;
-        this.projectionDialogService.openProjectDialogFor(this.message.getProjectionBuildDescriptor());
     }
 
     /**
