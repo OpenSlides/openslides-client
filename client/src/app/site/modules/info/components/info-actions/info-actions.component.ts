8d4159041e6dded678dbfbf1e64497ba06031a8b
Ludwig Reiter <ludwig.reiter@intevation.de>
2026-04-14 11:20:50 +0200

    Remove check-database buttons and presenter call

--- client/src/app/site/modules/info/components/info-actions/info-actions.component.ts
+++ client/src/app/site/modules/info/components/info-actions/info-actions.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { TranslateService } from '@ngx-translate/core';
-import { CheckDatabasePresenterService } from 'src/app/gateways/presenter/check-database-presenter.service';
 import { BaseComponent } from 'src/app/site/base/base.component';
 import { LifecycleService } from 'src/app/site/services/lifecycle.service';
 
@@ -14,8 +12,6 @@ import { LifecycleService } from 'src/app/site/services/lifecycle.service';
 export class InfoActionsComponent extends BaseComponent {
     public constructor(
         private lifecycleService: LifecycleService,
-        private snackbar: MatSnackBar,
-        private presenter: CheckDatabasePresenterService,
         protected override translate: TranslateService
     ) {
         super();
@@ -23,15 +19,5 @@ export class InfoActionsComponent extends BaseComponent {
 
     public resetCache(): void {
         this.lifecycleService.reset();
-    }
-
-    public async checkDatastore(all = false): Promise<void> {
-        const response = await this.presenter.call(all);
-        if (response.ok) {
-            this.snackbar.open(this.translate.instant(`Datastore is ok!`), `Ok`);
-        } else {
-            this.snackbar.open(this.translate.instant(`Datastore is corrupt! See the console for errors.`), `Ok`);
-            console.warn(response.errors);
-        }
     }
 }
