93d1dbd340f0af11608e894405978220d5ebf550
Ludwig Reiter <ludwig.reiter@intevation.de>
2026-04-14 15:27:03 +0200

    Remove check-database buttons and presenter call (#6082)

--- client/src/app/gateways/presenter/presenter.ts
+++ client/src/app/gateways/presenter/presenter.ts
@@ -9,7 +9,5 @@ export enum Presenter {
     GET_FORWARDING_MEETINGS = `get_forwarding_meetings`,
     SEARCH_USERS = `search_users`,
     SEARCH_DELETED_MODELS = `search_deleted_models`,
-    EXPORT_MEETING = `export_meeting`,
-    CHECK_DATABASE = `check_database`,
-    CHECK_DATABASE_ALL = `check_database_all`
+    EXPORT_MEETING = `export_meeting`
 }
