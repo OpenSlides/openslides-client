d5063922f6ba988272d1d5279664a83b6c1e612e
Ludwig Reiter <ludwig.reiter@intevation.de>
2026-04-24 10:00:31 +0200

    Cleanup of the account dialog main component (#6118)

--- client/src/app/site/modules/global-headbar/components/account-dialog-main/account-dialog-main.component.ts
+++ client/src/app/site/modules/global-headbar/components/account-dialog-main/account-dialog-main.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { Id } from 'src/app/domain/definitions/key-types';
-import { SubscriptionConfig } from 'src/app/domain/interfaces/subscription-config';
-import { BaseMeetingModelRequestHandler } from 'src/app/site/pages/meetings/base/base-meeting-model-request-handler.component';
+import { BaseModelRequestHandlerComponent } from 'src/app/site/base/base-model-request-handler.component';
 import { getDashboardMeetingListSubscriptionConfig } from 'src/app/site/pages/organization/pages/dashboard/dashboard.subscription';
 
 @Component({
@@ -10,8 +8,8 @@ import { getDashboardMeetingListSubscriptionConfig } from 'src/app/site/pages/or
     styleUrls: [`./account-dialog-main.component.scss`],
     standalone: false
 })
-export class AccountDialogMainComponent extends BaseMeetingModelRequestHandler {
-    protected getSubscriptions(_id: Id): SubscriptionConfig<any>[] {
-        return [getDashboardMeetingListSubscriptionConfig()];
+export class AccountDialogMainComponent extends BaseModelRequestHandlerComponent {
+    protected override onShouldCreateModelRequests(): void {
+        this.subscribeTo(getDashboardMeetingListSubscriptionConfig(), { hideWhenDestroyed: true });
     }
 }
