524b51a9a4126c3f1ac4e648b831d3cfdcc7d525
Bastian Rihm <brihm@intevation.de>
2026-04-30 14:31:25 +0200

    Cleanup motion poll component

--- client/src/app/site/pages/meetings/pages/motions/modules/motion-poll/components/motion-poll/motion-poll.component.ts
+++ client/src/app/site/pages/meetings/pages/motions/modules/motion-poll/components/motion-poll/motion-poll.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
@@ -6,7 +6,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterModule } from '@angular/router';
-import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+import { TranslatePipe } from '@ngx-translate/core';
 import { Id } from 'src/app/domain/definitions/key-types';
 import { Permission } from 'src/app/domain/definitions/permission';
 import { ProjectorButtonModule } from 'src/app/site/pages/meetings/modules/meetings-component-collector/projector-button/projector-button.module';
@@ -16,13 +16,10 @@ import { OperatorService } from 'src/app/site/services/operator.service';
 import { DirectivesModule } from 'src/app/ui/directives';
 
 import { ViewPoll } from '../../../../../polls';
-import { MotionPollService } from '../../services';
-import { MotionPollDetailContentComponent } from '../motion-poll-detail-content/motion-poll-detail-content.component';
 
 @Component({
     selector: `os-motion-poll`,
     imports: [
-        MotionPollDetailContentComponent,
         PollComponent,
         RouterModule,
         TranslatePipe,
@@ -39,6 +36,7 @@ import { MotionPollDetailContentComponent } from '../motion-poll-detail-content/
     styleUrls: [`./motion-poll.component.scss`]
 })
 export class MotionPollComponent extends BasePollComponent {
+    // TODO: use signals
     @Input()
     public set pollViewModel(poll: ViewPoll) {
         this.poll = poll;
@@ -65,46 +63,13 @@ export class MotionPollComponent extends BasePollComponent {
         return false;
     }
 
-    public get isAnonymous(): boolean {
-        return this.operator.isAnonymousLoggedIn;
-    }
-
-    public get canSeeVotes(): boolean {
-        /*
-        const option = this.poll.options[0];
-        return (
-            (this.poll.hasVotes && this.poll.stateHasVotes) ||
-            [option?.yes, option?.no, option?.abstain].some(value => value === VOTE_MAJORITY)
-        );
-        */
-
-        return this.poll.hasVotes;
-    }
-
-    public get isEVotingEnabled(): boolean {
-        return this.pollService.isElectronicVotingEnabled;
-    }
-
-    public get isPublished(): boolean {
-        return this.poll.isPublished;
-    }
-
     public get isSameMeeting(): boolean {
         return !this.poll.meeting_id || this.activeMeetingId === this.poll.meeting_id;
     }
 
-    public get canManagePoll(): boolean {
-        return this.operator.hasPerms(Permission.motionCanManagePolls);
-    }
+    private operator = inject(OperatorService);
 
-    public constructor(
-        protected override translate: TranslateService,
-        private pollService: MotionPollService,
-        private operator: OperatorService
-    ) {
-        super();
-    }
-
+    // TODO: Check if can removed
     public getDetailLink(): string {
         return `/${this.activeMeetingId}/motions/polls/${this.poll.sequential_number}`;
     }
