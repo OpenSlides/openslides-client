b24a9ac3ef24acd7c604ad4a8b893db7647e0cca
Bastian Rihm <brihm@intevation.de>
2026-05-04 18:26:34 +0200

    WIP: Selection poll result

--- client/src/app/site/pages/meetings/modules/poll/components/poll-result-selection/poll-result-selection.component.ts
+++ client/src/app/site/pages/meetings/modules/poll/components/poll-result-selection/poll-result-selection.component.ts
@@ -1,14 +1,86 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, input, signal } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+import { ThemeService } from 'src/app/site/services/theme.service';
+import { IconContainerComponent } from 'src/app/ui/modules/icon-container';
 
-import { ViewPoll } from '../../../../pages/polls';
+import { ViewPoll, ViewPollOption } from '../../../../pages/polls';
+import { PollParseNumberPipe, PollPercentBasePipe } from '../../pipes';
+import { ChartComponent, ChartData } from '../chart/chart.component';
+
+type Results = ResultRow[];
+interface ResultRow {
+    key: string;
+    votingOption: string;
+    color: string;
+    amount: number;
+    showPercent: boolean;
+}
+
+interface ResultsRaw {
+    [key: number]: string;
+    abstain: string;
+    invalid: number;
+}
+
+const PollChartBarThickness = 20;
 
 @Component({
     selector: 'os-poll-result-selection',
-    imports: [],
+    imports: [IconContainerComponent, PollPercentBasePipe, PollParseNumberPipe, ChartComponent, TranslatePipe],
     templateUrl: './poll-result-selection.component.html',
     styleUrl: './poll-result-selection.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PollResultSelectionComponent {
     public poll = input.required<ViewPoll>();
+
+    public shouldShowEntitled = signal<boolean>(false);
+
+    public themeService = inject(ThemeService);
+
+    public results = computed<ResultsRaw>(() => {
+        if (!this.poll().result) {
+            return [];
+        }
+
+        return JSON.parse(this.poll().result);
+    });
+
+    public options = computed<Results>(() => {
+        const results = this.results();
+        if (!results) {
+            return [];
+        }
+
+        const rows: Results = [];
+        for (const option of this.poll().options ?? []) {
+            rows.push({
+                key: option.fqid,
+                votingOption: `${option.meeting_user_id}`,
+                color: `circle`,
+                amount: +results[option.id] || 0,
+                showPercent: false
+            });
+        }
+
+        return rows;
+    });
+
+    public chartData = computed<ChartData>(() => {
+        const results = this.results();
+        if (!results) {
+            return [];
+        }
+
+        return Object.keys(results).map(key => {
+            return {
+                data: [+results[key]],
+                label: key,
+                backgroundColor: this.themeService.getPollColor(key),
+                hoverBackgroundColor: this.themeService.getPollColor(key),
+                barThickness: PollChartBarThickness,
+                maxBarThickness: PollChartBarThickness
+            };
+        });
+    });
 }
