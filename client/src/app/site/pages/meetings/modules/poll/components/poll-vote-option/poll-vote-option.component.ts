b53420248eaa245656267512fac651bb0b8da3f8
Bastian Rihm <brihm@intevation.de>
2026-05-05 12:34:48 +0200

    Vote meeting user options

--- client/src/app/site/pages/meetings/modules/poll/components/poll-vote-option/poll-vote-option.component.ts
+++ client/src/app/site/pages/meetings/modules/poll/components/poll-vote-option/poll-vote-option.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 import { ViewPollOption } from '../../../../pages/polls';
 
 @Component({
     selector: 'os-poll-vote-option',
-    imports: [],
+    imports: [TranslatePipe],
     templateUrl: './poll-vote-option.component.html',
     styleUrl: './poll-vote-option.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush
