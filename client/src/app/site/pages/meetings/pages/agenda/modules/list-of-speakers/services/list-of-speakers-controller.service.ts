3bf15ab8da401fbe16b67f7a6763edce5dcb3d53
Emanuel Schütze <emanuel@intevation.de>
2026-05-19 18:18:16 +0200

    Fix linter

--- client/src/app/site/pages/meetings/pages/agenda/modules/list-of-speakers/services/list-of-speakers-controller.service.ts
+++ client/src/app/site/pages/meetings/pages/agenda/modules/list-of-speakers/services/list-of-speakers-controller.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { _ } from '@ngx-translate/core';
 import { Id } from 'src/app/domain/definitions/key-types';
 import { ListOfSpeakers } from 'src/app/domain/models/list-of-speakers/list-of-speakers';
 import { ListOfSpeakersRepositoryService } from 'src/app/gateways/repositories/list-of-speakers/list-of-speakers-repository.service';
