5c01b13cb8b021ea1cd8bdd816f7a990e557f82b
Bastian Rihm <brihm@intevation.de>
2026-06-01 13:27:04 +0200

    Rating approval totals

--- client/src/app/site/pages/meetings/modules/poll/components/poll-result-rating-approval/poll-result-rating-approval.component.ts
+++ client/src/app/site/pages/meetings/modules/poll/components/poll-result-rating-approval/poll-result-rating-approval.component.ts
@@ -2,6 +2,7 @@ import { NgTemplateOutlet } from '@angular/common';
 import { ChangeDetectionStrategy, Component, computed } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
 import Big from 'big.js';
+import { RatingApprovalOnehundredPercentBase } from 'src/app/domain/models/poll/poll-config-rating-approval';
 
 import { RatingApprovalPollResult, ViewPollConfigRatingApproval, ViewPollOption } from '../../../../pages/polls';
 import { PollParseNumberPipe } from '../../pipes';
@@ -30,6 +31,10 @@ export class PollResultRatingApprovalComponent extends PollResultBaseComponent<
     ViewPollConfigRatingApproval,
     RatingApprovalPollResult
 > {
+    public onehundredPercentBase = computed<RatingApprovalOnehundredPercentBase>(() => {
+        return this.config().onehundred_percent_base;
+    });
+
     public resultOptions = computed<Results>(() => {
         const results = this.results();
         if (!results) {
@@ -66,5 +71,59 @@ export class PollResultRatingApprovalComponent extends PollResultBaseComponent<
         }
 
         return rows;
+    });
+
+    public totalVoteSum = computed<number>(() => {
+        return this.config().totalVotes!;
+    });
+
+    public validBallots = computed<number | null>(() => {
+        if (this.onehundredPercentBase() === `cast`) {
+            return null;
+        }
+
+        return this.config().validBallots;
+    });
+
+    public validBallotsPercent = computed<string | null>(() => {
+        if (!this.config().onehundredPercentBaseNum) {
+            return null;
+        }
+
+        return this.formatResultDecimal((this.validBallots() / this.config().onehundredPercentBaseNum) * 100);
+    });
+
+    public invalidBallots = computed<number | null>(() => {
+        if (this.onehundredPercentBase() === `cast`) {
+            return null;
+        }
+
+        return this.config().invalidBallots;
+    });
+
+    public invalidBallotsPercent = computed<string | null>(() => {
+        if (!this.config().onehundredPercentBaseNum) {
+            return null;
+        }
+
+        return this.formatResultDecimal((this.invalidBallots() / this.config().onehundredPercentBaseNum) * 100);
+    });
+
+    public castedBallots = computed<number | null>(() => {
+        if (this.onehundredPercentBase() !== `cast`) {
+            return null;
+        }
+
+        return this.poll().ballot_ids.length;
+    });
+
+    public entitledUsers = computed<number | null>(() => {
+        // TODO: Implement if available
+        return null;
+    });
+
+    public presentEntitledUsers = computed<number | null>(() => {
+        // TODO: Implement if available
+        return null;
     });
 }
