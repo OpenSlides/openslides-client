d8abf5132f1be48b25a1332ec2f37b7f74450a3d
Elblinator <lelble@intevation.de>
2026-06-03 08:17:03 +0200

    Add onDestroy and load poll

--- client/src/app/site/pages/meetings/modules/poll/components/poll-detail/poll-detail.component.ts
+++ client/src/app/site/pages/meetings/modules/poll/components/poll-detail/poll-detail.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, inject, signal } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -6,10 +6,12 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { ActivatedRoute } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 import { BaseComponent } from 'src/app/site/base/base.component';
 import { HeadBarModule } from 'src/app/ui/modules/head-bar';
 import { PipesModule } from 'src/app/ui/pipes';
 
+import { ViewPoll } from '../../../../pages/polls';
 import { PollControllerService } from '../../services/poll-controller.service';
 import { VotingService } from '../../services/voting.service';
 import { PollComponent } from '../poll/poll.component';
@@ -31,18 +33,28 @@ import { PollComponent } from '../poll/poll.component';
     ]
 })
 export class PollDetailComponent extends BaseComponent {
-    public pollID = signal(-1);
+    private pollID = signal(-1);
+    public poll: ViewPoll | null;
 
     public override translate = inject(TranslateService);
     public pollRepo = inject(PollControllerService);
     public votingService = inject(VotingService);
     private activatedRoute = inject(ActivatedRoute);
+    private destroyRef = inject(DestroyRef);
+
+    private subscription: Subscription;
 
     public constructor() {
         super();
         super.setTitle(`Singular poll`);
-        this.activatedRoute.params.subscribe(p => {
+        this.subscription = this.activatedRoute.params.subscribe(p => {
             this.pollID.set(p['id']);
+        });
+        this.poll = this.pollRepo.getViewModel(this.pollID());
+
+        this.destroyRef.onDestroy(() => {
+            this.subscription?.unsubscribe();
+            this.subscription = null;
         });
     }
 }
