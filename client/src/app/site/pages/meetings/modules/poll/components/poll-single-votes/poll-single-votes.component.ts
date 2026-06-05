3273e654231efb11ef7ae8bd5d5f9ab270836b51
Elblinator <lelble@intevation.de>
2026-06-05 15:45:14 +0200

    Update detail view

--- client/src/app/site/pages/meetings/modules/poll/components/poll-single-votes/poll-single-votes.component.ts
+++ client/src/app/site/pages/meetings/modules/poll/components/poll-single-votes/poll-single-votes.component.ts
@@ -1,5 +1,4 @@
-import { ChangeDetectionStrategy, Component, computed, inject, input, Signal, signal } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { ChangeDetectionStrategy, Component, computed, effect, inject, input, signal } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -7,15 +6,19 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { ActivatedRoute, RouterLink } from '@angular/router';
 import { TranslatePipe, TranslateService } from '@ngx-translate/core';
-import { map } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { PollState } from 'src/app/domain/models/poll';
 import { BaseComponent } from 'src/app/site/base/base.component';
+import { DirectivesModule } from 'src/app/ui/directives';
 import { HeadBarModule } from 'src/app/ui/modules/head-bar';
+import { ListModule } from 'src/app/ui/modules/list';
 import { PipesModule } from 'src/app/ui/pipes';
 
 import { ViewPoll } from '../../../../pages/polls';
+import { BaseVoteData } from '../../base/base-poll-detail.component';
 import { PollControllerService } from '../../services/poll-controller.service';
+import { VotesFilterService } from '../../services/votes-filter.service';
 import { VotingService } from '../../services/voting.service';
 
 @Component({
@@ -25,6 +28,8 @@ import { VotingService } from '../../services/voting.service';
     changeDetection: ChangeDetectionStrategy.OnPush,
     imports: [
         TranslatePipe,
+        DirectivesModule,
+        ListModule,
         HeadBarModule,
         MatInputModule,
         MatFormFieldModule,
@@ -47,9 +52,30 @@ export class PollSingleVotesComponent extends BaseComponent {
 
     public selectedTab = signal(0);
 
+    public votesDataSubject: Subject<BaseVoteData[]>;
+
+    public votesDataObservable: Observable<BaseVoteData[]>;
+
     public override translate = inject(TranslateService);
     public pollRepo = inject(PollControllerService);
     public votingService = inject(VotingService);
+    public filter = inject(VotesFilterService);
+
+    public constructor() {
+        super();
+
+        this.votesDataSubject = new Subject<BaseVoteData[]>();
+
+        effect(() => {
+            if (this.poll().state === PollState.Finished) {
+                this.votesDataSubject.next([]);
+            } else {
+                this.votesDataSubject.next([]);
+            }
+        });
+
+        this.votesDataObservable = this.votesDataSubject;
+    }
 
     public printShit(): void {
         console.log(this.poll());
@@ -59,5 +85,6 @@ export class PollSingleVotesComponent extends BaseComponent {
         console.log(this.poll().poll);
         console.log(this.poll().result);
         console.log(this.poll().options);
+        console.log(this.poll().state);
     }
 }
