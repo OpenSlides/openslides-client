6c8f76702bf0affa85e6a61b524515812289bb09
Elblinator <lelble@intevation.de>
2026-06-12 12:11:58 +0200

    Clean-up entitled users component

--- client/src/app/site/pages/meetings/modules/poll/components/poll-entitled-users/poll-entitled-user.component.ts
+++ client/src/app/site/pages/meetings/modules/poll/components/poll-entitled-users/poll-entitled-user.component.ts
@@ -1,59 +1,18 @@
-import { ChangeDetectionStrategy, Component, computed, inject, input, signal } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { TranslateService } from '@ngx-translate/core';
+import { ChangeDetectionStrategy, Component, input, signal } from '@angular/core';
 import { BaseComponent } from 'src/app/site/base/base.component';
-import { HeadBarModule } from 'src/app/ui/modules/head-bar';
-import { PipesModule } from 'src/app/ui/pipes';
 
 import { ViewPoll } from '../../../../pages/polls';
-import { PollControllerService } from '../../services/poll-controller.service';
-import { VotingService } from '../../services/voting.service';
 
 @Component({
     selector: `os-poll-entitled-user`,
     templateUrl: `./poll-entitled-user.component.html`,
     styleUrls: [`./poll-entitled-user.component.scss`],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [
-        HeadBarModule,
-        MatInputModule,
-        MatFormFieldModule,
-        MatCheckboxModule,
-        MatSelectModule,
-        MatCardModule,
-        MatTooltipModule,
-        MatTabsModule,
-        PipesModule
-    ]
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PollEntitledUserComponent extends BaseComponent {
     public poll = input.required<ViewPoll>();
 
-    public getDetailLink = computed(() => {
-        return `/${this.poll().getDetailStateUrl()}`;
-    });
-
     public filterProps = [`user.getFullName`];
 
     public selectedTab = signal(0);
-
-    public override translate = inject(TranslateService);
-    public pollRepo = inject(PollControllerService);
-    public votingService = inject(VotingService);
-
-    public printShit(): void {
-        console.log(this.poll());
-        console.log(this.poll().getContentObject());
-        console.log(this.poll().getContentObject().collection);
-        console.log(this.poll().getDetailStateUrl());
-        console.log(this.poll().poll);
-        console.log(this.poll().result);
-        console.log(this.poll().options);
-    }
 }
