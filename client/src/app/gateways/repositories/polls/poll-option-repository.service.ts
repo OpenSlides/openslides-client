4e833985902ce5ad55271af03037388ee274010d
Bastian Rihm <brihm@intevation.de>
2026-06-23 12:24:20 +0200

    Fix translations

--- client/src/app/gateways/repositories/polls/poll-option-repository.service.ts
+++ client/src/app/gateways/repositories/polls/poll-option-repository.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { _ } from '@ngx-translate/core';
 import { PollOption } from 'src/app/domain/models/poll/poll-option';
 import { ViewPollOption } from 'src/app/site/pages/meetings/pages/polls/view-models/poll-option';
 
@@ -16,5 +17,6 @@ export class PollOptionRepositoryService extends BaseMeetingRelatedRepository<Vi
     public getTitle = (viewPollOption: ViewPollOption): string =>
         viewPollOption.meeting_user_id ? viewPollOption.meeting_user.getTitle() : viewPollOption.text;
 
-    public getVerboseName = (plural = false): string => this.translate.instant(plural ? `Poll options` : `Poll option`);
+    public getVerboseName = (plural = false): string =>
+        this.translate.instant(plural ? _(`Poll options`) : _(`Poll option`));
 }
