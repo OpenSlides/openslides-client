9ecebc0908a4c5b716406b7fa26cf64142c093cc
Javier Borrego <javier.borrego@intevation.de>
2026-07-27 10:04:04 +0200

    Solve conflicts

--- client/src/app/site/modules/global-headbar/components/account-button/account-button.component.ts
+++ client/src/app/site/modules/global-headbar/components/account-button/account-button.component.ts
@@ -5,6 +5,7 @@ import { Router } from '@angular/router';
 import { Id } from '@app/domain/definitions/key-types';
 import { allAvailableTranslations, availableTranslations } from '@app/domain/definitions/languages';
 import { getOmlVerboseName } from '@app/domain/definitions/organization-permission';
+import { Permission } from '@app/domain/definitions/permission';
 import { largeDialogSettings } from '@app/infrastructure/utils/dialog-settings';
 import { mediumDialogSettings } from '@app/infrastructure/utils/dialog-settings';
 import { ActiveMeetingIdService } from '@app/site/pages/meetings/services/active-meeting-id.service';
@@ -19,23 +20,6 @@ import { ChessDialogComponent } from '@app/ui/modules/sidenav/modules/easter-egg
 import { ChessChallengeService } from '@app/ui/modules/sidenav/modules/easter-egg/modules/chess-dialog/services/chess-challenge.service';
 import { TranslateService } from '@ngx-translate/core';
 import { Observable, Subscription } from 'rxjs';
-import { Id } from 'src/app/domain/definitions/key-types';
-import { allAvailableTranslations, availableTranslations } from 'src/app/domain/definitions/languages';
-import { getOmlVerboseName } from 'src/app/domain/definitions/organization-permission';
-import { Permission } from 'src/app/domain/definitions/permission';
-import { largeDialogSettings } from 'src/app/infrastructure/utils/dialog-settings';
-import { mediumDialogSettings } from 'src/app/infrastructure/utils/dialog-settings';
-import { ParticipantListInfoDialogService } from 'src/app/site/pages/meetings/pages/participants/pages/participant-list/modules/participant-list-info-dialog';
-import { ActiveMeetingIdService } from 'src/app/site/pages/meetings/services/active-meeting-id.service';
-import { MeetingSettingsService } from 'src/app/site/pages/meetings/services/meeting-settings.service';
-import { ViewUser } from 'src/app/site/pages/meetings/view-models/view-user';
-import { AuthService } from 'src/app/site/services/auth.service';
-import { OperatorService } from 'src/app/site/services/operator.service';
-import { ThemeService } from 'src/app/site/services/theme.service';
-import { UserControllerService } from 'src/app/site/services/user-controller.service';
-import { BaseUiComponent } from 'src/app/ui/base/base-ui-component';
-import { ChessDialogComponent } from 'src/app/ui/modules/sidenav/modules/easter-egg/modules/chess-dialog/components/chess-dialog/chess-dialog.component';
-import { ChessChallengeService } from 'src/app/ui/modules/sidenav/modules/easter-egg/modules/chess-dialog/services/chess-challenge.service';
 
 import { AccountDialogMainComponent } from '../account-dialog-main/account-dialog-main.component';
 
@@ -250,35 +234,6 @@ export class AccountButtonComponent extends BaseUiComponent implements OnInit {
             stringForUserPresent = this.translate.instant(`Your account is not in this meeting.`);
         }
         return stringForUserPresent.replace(`{}`, this.user.short_name);
-    }
-
-    public canEditOwnDelegation(user: ViewUser): boolean {
-        if (
-            this.operator.hasPerms(Permission.userCanEditOwnDelegation) &&
-            !this.operator.hasPerms(Permission.userCanManage) &&
-            !this.operator.hasPerms(Permission.userCanUpdate)
-        ) {
-            return this.operator.operatorId === user.id;
-        } else if (
-            this.operator.hasPerms(Permission.userCanManage) ||
-            this.operator.hasPerms(Permission.userCanUpdate)
-        ) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public async openDialogEditInfo(user: ViewUser): Promise<void> {
-        await this.participantListDialog.open({
-            user,
-            id: user.id,
-            name: user.getName(),
-            number: user.number(),
-            structure_level_ids: this.user.structure_level_ids(),
-            vote_delegations_from_ids: this.user.vote_delegations_from_meeting_user_ids(),
-            vote_delegated_to_id: this.user.vote_delegated_to_meeting_user_id()
-        });
     }
 
     public canEditOwnDelegation(user: ViewUser): boolean {
--- client/src/app/site/pages/meetings/pages/participants/pages/participant-list/modules/participant-list-info-dialog/services/participant-list-info-dialog.service.ts
+++ client/src/app/site/pages/meetings/pages/participants/pages/participant-list/modules/participant-list-info-dialog/services/participant-list-info-dialog.service.ts
@@ -1,9 +1,9 @@
 import { Service } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
 import { Id } from '@app/domain/definitions/key-types';
-import { Identifiable } from '@app/domain/interfaces';
 import { infoDialogSettings } from '@app/infrastructure/utils/dialog-settings';
+import { ViewUser } from '@app/site/pages/meetings/view-models/view-user';
 import { BaseDialogService } from '@app/ui/base/base-dialog-service';
+import { firstValueFrom } from 'rxjs';
 
 import { ParticipantControllerService } from '../../../../../services/common/participant-controller.service';
 import { ParticipantListInfoDialogComponent } from '../components/participant-list-info-dialog/participant-list-info-dialog.component';
