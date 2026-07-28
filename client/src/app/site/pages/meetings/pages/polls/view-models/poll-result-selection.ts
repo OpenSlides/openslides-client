91d0bd3c9d90336e453223336b40bd879f37b57e
Bastian Rihm <brihm@intevation.de>
2026-07-28 10:11:27 +0200

    Fix strike out results

--- client/src/app/site/pages/meetings/pages/polls/view-models/poll-result-selection.ts
+++ client/src/app/site/pages/meetings/pages/polls/view-models/poll-result-selection.ts
@@ -1,3 +1,5 @@
+import Big from 'big.js';
+
 import { BasePollResult } from './base-poll-result';
 import { ViewPollConfigSelection } from './poll-config-selection';
 
@@ -5,4 +7,24 @@ export class SelectionPollResult extends BasePollResult<ViewPollConfigSelection,
     [key: number]: string;
     public nota?: string;
     public abstain?: string;
+
+    public constructor(
+        public override config: ViewPollConfigSelection,
+        input: Partial<SelectionPollResult>
+    ) {
+        super(config, input);
+
+        if (config.strike_out) {
+            const validVotes = this.total_ballots - (this.invalid ?? 0) - +(this.abstain ?? 0);
+
+            const options = config.poll?.options;
+            if (options) {
+                for (const option of options) {
+                    this[option.id.toString()] = Big(validVotes)
+                        .sub(Big(this[option.id.toString()] || 0))
+                        .toString();
+                }
+            }
+        }
+    }
 }
