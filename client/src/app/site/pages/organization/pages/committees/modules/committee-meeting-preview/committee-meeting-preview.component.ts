da3c5a7673956bc390820a285aa0279ff5215cc8
Elblinator <lelble@intevation.de>
2026-07-29 14:53:42 +0200

    Add manual close for ocmmitt-detail-view

--- client/src/app/site/pages/organization/pages/committees/modules/committee-meeting-preview/committee-meeting-preview.component.ts
+++ client/src/app/site/pages/organization/pages/committees/modules/committee-meeting-preview/committee-meeting-preview.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject, Input, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, Input, OnDestroy, OnInit, viewChild, ViewEncapsulation } from '@angular/core';
 import { CML, OML } from '@app/domain/definitions/organization-permission';
 import { MeetingControllerService } from '@app/site/pages/meetings/services/meeting-controller.service';
 import { ViewMeeting } from '@app/site/pages/meetings/view-models/view-meeting';
@@ -10,6 +10,7 @@ import { Subscription } from 'rxjs';
 
 import { ViewCommittee } from '../../view-models';
 import { MeetingService } from '../services/meeting.service';
+import { MatMenuTrigger } from '@angular/material/menu';
 
 @Component({
     selector: `os-committee-meeting-preview`,
@@ -23,6 +24,8 @@ export class CommitteeMeetingPreviewComponent implements OnDestroy, OnInit {
     @Input() public meeting!: ViewMeeting;
     @Input() public committee!: ViewCommittee;
     @Input() public isCMAndRequireDuplicateFrom!: boolean;
+
+    public menuTrigger = viewChild<MatMenuTrigger>('trigger');
 
     public readonly OML = OML;
     public readonly CML = CML;
@@ -127,6 +130,7 @@ export class CommitteeMeetingPreviewComponent implements OnDestroy, OnInit {
     }
 
     public async onDuplicate(): Promise<void> {
+        this.menuTrigger().closeMenu();
         const title = this.translate.instant(`Are you sure you want to duplicate this meeting?`);
         const content = this.title;
 
