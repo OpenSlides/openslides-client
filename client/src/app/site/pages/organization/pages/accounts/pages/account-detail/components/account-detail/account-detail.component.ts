3d046f3147d4ca5ce649ae35abf70d4009028fde
Elblinator <lelble@intevation.de>
2026-08-04 14:56:56 +0200

    Fix Account Admin if home_committee is set

--- client/src/app/site/pages/organization/pages/accounts/pages/account-detail/components/account-detail/account-detail.component.ts
+++ client/src/app/site/pages/organization/pages/accounts/pages/account-detail/components/account-detail/account-detail.component.ts
@@ -107,32 +107,28 @@ export class AccountDetailComponent extends BaseComponent implements OnInit {
     public get canManageHomeCommittee(): boolean {
         return this.home_committee_id
             ? this.operator.hasCommitteePermissions(this.home_committee_id, CML.can_manage)
-            : this.operator.hasOrganizationPermissions(OML.can_manage_users) || this.operator.isAnyCommitteeManager;
+            : this.operator.hasOrganizationPermissions(OML.can_manage_organization) || this.operator.isAnyCommitteeManager;
     }
 
     public get comitteeAdministrationAmount(): number {
         return Object.values(this._tableData).filter(row => row[`is_manager`] === true).length;
     }
 
-    public get userOML(): OML {
-        return this.user?.organization_management_level as OML;
-    }
-
     public get canEdit(): boolean {
-        if (!this.userOML) {
+        if (!this.operator.userOML) {
             return true;
         }
-        return this.operator.hasOrganizationPermissions(this.userOML);
+        return this.operator.hasOrganizationPermissions(this.operator.userOML);
     }
 
     public shouldEnableFormControl(): boolean {
         if (
-            !this.userOML &&
+            !this.operator.userOML &&
             (!this.home_committee_id || this.operator.hasCommitteePermissions(this.home_committee_id, CML.can_manage))
         ) {
             return true;
         }
-        return this.operator.hasOrganizationPermissions(this.userOML);
+        return this.operator.hasOrganizationPermissions(this.operator.userOML);
     }
 
     public shouldEnableFormControlFn: (_: string) => boolean = (_: string) => this.shouldEnableFormControl();
@@ -148,9 +144,9 @@ export class AccountDetailComponent extends BaseComponent implements OnInit {
 
     public readonly committeeController = inject(CommitteeControllerService);
     public readonly committeeSortService = inject(CommitteeSortService);
+    public readonly operator = inject(OperatorService);
     private route = inject(ActivatedRoute);
     private osRouter = inject(OpenSlidesRouterService);
-    private operator = inject(OperatorService);
     private accountController = inject(AccountControllerService);
     private userController = inject(UserControllerService);
     private promptService = inject(PromptService);
@@ -368,6 +364,16 @@ export class AccountDetailComponent extends BaseComponent implements OnInit {
             payload[`committee_management_ids`] = undefined;
             payload[`organization_management_level`] = undefined;
         }
+        if (this.operator.userOML === OML.can_manage_users) {
+            delete payload.home_committee_id
+        }
+        if (payload.home_committee_id === 0) {
+            if (isCreate) {
+                delete payload.home_committee_id;
+            } else {
+                payload.home_committee_id = null;
+            }
+        }
         if (payload.member_number === ``) {
             if (isCreate) {
                 delete payload.member_number;
@@ -380,13 +386,6 @@ export class AccountDetailComponent extends BaseComponent implements OnInit {
                 delete payload.gender_id;
             } else {
                 payload.gender_id = null;
-            }
-        }
-        if (payload.home_committee_id === 0) {
-            if (isCreate) {
-                delete payload.home_committee_id;
-            } else {
-                payload.home_committee_id = null;
             }
         }
         return payload;
