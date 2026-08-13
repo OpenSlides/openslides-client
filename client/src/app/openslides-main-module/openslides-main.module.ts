180e5d0b717ca41925937408ef7515e863c158b6
Bastian Rihm <brihm@intevation.de>
2026-08-13 12:09:56 +0200

    Support for translation string interpolation (#6500)

--- client/src/app/openslides-main-module/openslides-main.module.ts
+++ client/src/app/openslides-main-module/openslides-main.module.ts
@@ -5,7 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { GlobalSpinnerModule } from '@app/site/modules/global-spinner';
-import { provideTranslateService, TranslateLoader, TranslateParser } from '@ngx-translate/core';
+import { CustomMissingTranslationHandler } from '@app/site/modules/translations/missing-translation-handler';
+import {
+    provideMissingTranslationHandler,
+    provideTranslateService,
+    TranslateLoader,
+    TranslateParser
+} from '@ngx-translate/core';
 
 import { environment } from '../../environments/environment';
 import { CustomTranslationService } from '../site/modules/translations/custom-translation.service';
@@ -66,6 +72,7 @@ if (isFirefox && `serviceWorker` in navigator) {
                 useClass: PruningTranslationLoader,
                 deps: [HttpClient]
             },
+            missingTranslationHandler: provideMissingTranslationHandler(CustomMissingTranslationHandler),
             parser: {
                 provide: TranslateParser,
                 useClass: CustomTranslationParser,
