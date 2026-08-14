50a0fb96ff16c50722533cd5d7224bb73a4c3411
Bastian Rihm <brihm@intevation.de>
2026-08-14 14:34:36 +0200

    Remove BrowserAnimationsModule (#6502)

--- client/src/app/openslides-main-module/openslides-main.module.ts
+++ client/src/app/openslides-main-module/openslides-main.module.ts
@@ -2,7 +2,6 @@ import { HttpClient, provideHttpClient, withInterceptorsFromDi, withXhr } from '
 import { inject, NgModule, provideAppInitializer } from '@angular/core';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { GlobalSpinnerModule } from '@app/site/modules/global-spinner';
 import { CustomMissingTranslationHandler } from '@app/site/modules/translations/missing-translation-handler';
@@ -48,7 +47,6 @@ if (isFirefox && `serviceWorker` in navigator) {
     imports: [
         BrowserModule,
         OpenSlidesMainRoutingModule,
-        BrowserAnimationsModule,
         ...NOT_LAZY_LOADED_MODULES,
         ServiceWorkerModule.register(`sw.js`, {
             enabled: environment.production && !isFirefox,
